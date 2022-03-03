6bb8eeb620f714fb98b8352fec01e4be29c0c0fc
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-03 17:02:38 +0000

    Show ShoppingCartScreen with fullscreenDialog: true

--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final goRouter = GoRouter(
@@ -12,7 +13,11 @@ final goRouter = GoRouter(
       routes: [
         GoRoute(
           path: 'cart',
-          builder: (context, state) => const ShoppingCartScreen(),
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const ShoppingCartScreen(),
+          ),
         ),
       ],
     ),
