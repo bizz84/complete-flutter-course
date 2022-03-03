5ec62b29f87da0b336362b2b79c686b6f10ea447
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-03 12:43:15 +0000

    Navigate to the shopping cart using GoRouter

---
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
+import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final goRouter = GoRouter(
+  initialLocation: '/',
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const ProductsListScreen(),
+      routes: [
+        GoRoute(
+          path: 'cart',
+          builder: (context, state) => const ShoppingCartScreen(),
+        ),
+      ],
+    ),
+  ],
+);
