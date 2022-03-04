2397f48321f06640f296413bcd3e960bd2c6df9a
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-04 11:03:19 +0000

    Add the orders, account, signIn routes

--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -1,5 +1,9 @@
+import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -17,6 +21,32 @@ final goRouter = GoRouter(
             key: state.pageKey,
             fullscreenDialog: true,
             child: const ShoppingCartScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'orders',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const OrdersListScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'account',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const AccountScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'signIn',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const EmailPasswordSignInScreen(
+              formType: EmailPasswordSignInFormType.signIn,
+            ),
           ),
         ),
       ],
