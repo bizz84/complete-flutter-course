902fb256e0fe10f1e8bf135b569414de0fdca1f5
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-04 12:01:32 +0000

    Routing by name

--- ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
+++ ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/models/app_user.dart';
 import 'package:go_router/go_router.dart';
@@ -50,13 +51,13 @@ class MoreMenuButton extends StatelessWidget {
         // push to different routes based on selected option
         switch (option) {
           case PopupMenuOption.signIn:
-            context.go('/signIn');
+            context.goNamed(AppRoute.signIn.name);
             break;
           case PopupMenuOption.orders:
-            context.go('/orders');
+            context.goNamed(AppRoute.orders.name);
             break;
           case PopupMenuOption.account:
-            context.go('/account');
+            context.goNamed(AppRoute.account.name);
             break;
         }
       },
