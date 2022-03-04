c060c7a6abc8514a054a2979994a4b045a0622f4
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-04 11:03:19 +0000

    Add the orders, account, signIn routes

--- ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
+++ ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
@@ -1,10 +1,7 @@
-import 'package:ecommerce_app/src/features/account/account_screen.dart';
-import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/models/app_user.dart';
+import 'package:go_router/go_router.dart';
 
 enum PopupMenuOption {
   signIn,
@@ -53,30 +50,13 @@ class MoreMenuButton extends StatelessWidget {
         // push to different routes based on selected option
         switch (option) {
           case PopupMenuOption.signIn:
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                fullscreenDialog: true,
-                builder: (_) => const EmailPasswordSignInScreen(
-                  formType: EmailPasswordSignInFormType.signIn,
-                ),
-              ),
-            );
+            context.go('/signIn');
             break;
           case PopupMenuOption.orders:
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                fullscreenDialog: true,
-                builder: (_) => const OrdersListScreen(),
-              ),
-            );
+            context.go('/orders');
             break;
           case PopupMenuOption.account:
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                fullscreenDialog: true,
-                builder: (_) => const AccountScreen(),
-              ),
-            );
+            context.go('/account');
             break;
         }
       },
