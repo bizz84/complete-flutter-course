ecc3497634dd4deeeb3a8f4b7cc6200d3e34fc95
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-07 11:00:29 +0000

    Routing with pathParameters

--- ecommerce_app/lib/src/features/products_list/products_grid.dart
+++ ecommerce_app/lib/src/features/products_list/products_grid.dart
@@ -1,12 +1,13 @@
 import 'dart:math';
 
 import 'package:ecommerce_app/src/constants/test_products.dart';
-import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/features/products_list/product_card.dart';
+import 'package:go_router/go_router.dart';
 
 /// A widget that displays the list of products that match the search query.
 class ProductsGrid extends StatelessWidget {
@@ -29,10 +30,9 @@ class ProductsGrid extends StatelessWidget {
               final product = products[index];
               return ProductCard(
                 product: product,
-                onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => ProductScreen(productId: product.id),
-                  ),
+                onPressed: () => context.goNamed(
+                  AppRoute.product.name,
+                  pathParameters: {'id': product.id},
                 ),
               );
             },
