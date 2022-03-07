d591ebe911a2609e7ad1fd44ff8f198fd06c1c43
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-07 12:27:42 +0000

    Replace goNamed with pushNamed

--- ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
+++ ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
@@ -51,13 +51,13 @@ class MoreMenuButton extends StatelessWidget {
         // push to different routes based on selected option
         switch (option) {
           case PopupMenuOption.signIn:
-            context.goNamed(AppRoute.signIn.name);
+            context.pushNamed(AppRoute.signIn.name);
             break;
           case PopupMenuOption.orders:
-            context.goNamed(AppRoute.orders.name);
+            context.pushNamed(AppRoute.orders.name);
             break;
           case PopupMenuOption.account:
-            context.goNamed(AppRoute.account.name);
+            context.pushNamed(AppRoute.account.name);
             break;
         }
       },
