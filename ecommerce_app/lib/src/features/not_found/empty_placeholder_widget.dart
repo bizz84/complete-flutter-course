741929d142a6763103fccf49e3d84b337ac6a484
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-07 11:32:19 +0000

    Add errorBuilder

--- ecommerce_app/lib/src/features/not_found/empty_placeholder_widget.dart
+++ ecommerce_app/lib/src/features/not_found/empty_placeholder_widget.dart
@@ -1,7 +1,9 @@
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:go_router/go_router.dart';
 
 /// Placeholder widget showing a message and CTA to go back to the home screen.
 class EmptyPlaceholderWidget extends StatelessWidget {
@@ -24,10 +26,7 @@ class EmptyPlaceholderWidget extends StatelessWidget {
             ),
             gapH32,
             PrimaryButton(
-              onPressed: () =>
-                  // * Pop all routes in the navigation stack until the home
-                  // * screen is reached.
-                  Navigator.of(context).popUntil((route) => route.isFirst),
+              onPressed: () => context.goNamed(AppRoute.home.name),
               text: 'Go Home'.hardcoded,
             )
           ],
