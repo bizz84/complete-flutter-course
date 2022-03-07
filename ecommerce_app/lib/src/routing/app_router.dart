f02c10a908e8195fc18c0a7593c571f48001519e
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-07 11:00:29 +0000

    Routing with pathParameters

--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/src/features/account/account_screen.dart';
 import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
+import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
 import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
@@ -9,6 +10,7 @@ import 'package:go_router/go_router.dart';
 
 enum AppRoute {
   home,
+  product,
   cart,
   orders,
   account,
@@ -24,6 +26,14 @@ final goRouter = GoRouter(
       name: AppRoute.home.name,
       builder: (context, state) => const ProductsListScreen(),
       routes: [
+        GoRoute(
+          path: 'product/:id',
+          name: AppRoute.product.name,
+          builder: (context, state) {
+            final productId = state.pathParameters['id']!;
+            return ProductScreen(productId: productId);
+          },
+        ),
         GoRoute(
           path: 'cart',
           name: AppRoute.cart.name,
