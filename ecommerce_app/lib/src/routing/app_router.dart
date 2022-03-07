5156a54be7f1afb4fa51308f4348e30df6949b47
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-07 14:40:49 +0000

    Add leaveReview and checkout routes

--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/checkout/checkout_screen.dart';
+import 'package:ecommerce_app/src/features/leave_review_page/leave_review_screen.dart';
 import 'package:ecommerce_app/src/features/not_found/not_found_screen.dart';
 import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
 import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
@@ -12,7 +14,9 @@ import 'package:go_router/go_router.dart';
 enum AppRoute {
   home,
   product,
+  leaveReview,
   cart,
+  checkout,
   orders,
   account,
   signIn,
@@ -34,6 +38,20 @@ final goRouter = GoRouter(
             final productId = state.pathParameters['id']!;
             return ProductScreen(productId: productId);
           },
+          routes: [
+            GoRoute(
+              path: 'review',
+              name: AppRoute.leaveReview.name,
+              pageBuilder: (context, state) {
+                final productId = state.pathParameters['id']!;
+                return MaterialPage(
+                  key: state.pageKey,
+                  fullscreenDialog: true,
+                  child: LeaveReviewScreen(productId: productId),
+                );
+              },
+            ),
+          ],
         ),
         GoRoute(
           path: 'cart',
@@ -43,6 +61,17 @@ final goRouter = GoRouter(
             fullscreenDialog: true,
             child: const ShoppingCartScreen(),
           ),
+          routes: [
+            GoRoute(
+              path: 'checkout',
+              name: AppRoute.checkout.name,
+              pageBuilder: (context, state) => MaterialPage(
+                key: state.pageKey,
+                fullscreenDialog: true,
+                child: const CheckoutScreen(),
+              ),
+            ),
+          ],
         ),
         GoRoute(
           path: 'orders',
