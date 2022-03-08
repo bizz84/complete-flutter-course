927724fdee407a154058691ac21657c0a96d7a7e
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-08 10:30:28 +0000

    Uncomment code in AccountScreen

--- ecommerce_app/lib/src/features/account/account_screen.dart
+++ ecommerce_app/lib/src/features/account/account_screen.dart
@@ -19,17 +19,16 @@ class AccountScreen extends StatelessWidget {
           ActionTextButton(
             text: 'Logout'.hardcoded,
             onPressed: () async {
-              showNotImplementedAlertDialog(context: context);
-              // final logout = await showAlertDialog(
-              //   context: context,
-              //   title: 'Are you sure?'.hardcoded,
-              //   cancelActionText: 'Cancel'.hardcoded,
-              //   defaultActionText: 'Logout'.hardcoded,
-              // );
-              // if (logout == true) {
-              //   // TODO: Sign out the user.
-              //   Navigator.of(context).pop();
-              // }
+              final logout = await showAlertDialog(
+                context: context,
+                title: 'Are you sure?'.hardcoded,
+                cancelActionText: 'Cancel'.hardcoded,
+                defaultActionText: 'Logout'.hardcoded,
+              );
+              if (logout == true) {
+                // TODO: Sign out the user.
+                Navigator.of(context).pop();
+              }
             },
           ),
         ],
