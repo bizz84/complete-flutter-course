b2c2526511f07e7318d7166cccef9037af16f2b2
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-21 13:20:47 +0000

    Move to "feature-first" project structure (#7)

--- ecommerce_app/lib/src/constants/test_products.dart
+++ ecommerce_app/lib/src/constants/test_products.dart
@@ -1,4 +1,4 @@
-import 'package:ecommerce_app/src/models/product.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
 
 /// Test products to be used until a data source is implemented
 const kTestProducts = [
---
+++ ecommerce_app/lib/src/features/cart/domain/item.dart
@@ -0,0 +1,11 @@
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+
+/// A product along with a quantity that can be added to an order/cart
+class Item {
+  const Item({
+    required this.productId,
+    required this.quantity,
+  });
+  final ProductID productId;
+  final int quantity;
+}
