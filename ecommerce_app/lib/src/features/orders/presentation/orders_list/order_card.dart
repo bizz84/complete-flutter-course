19f9f7526a6e8a49dfc39927a165dd7935b2a7d9
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-21 13:20:47 +0000

    Move to "feature-first" project structure (#7)
    # Conflicts:
    #	ecommerce_app/lib/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart

---
+++ ecommerce_app/lib/src/features/orders/presentation/orders_list/order_card.dart
@@ -0,0 +1,108 @@
+import 'package:ecommerce_app/src/features/orders/presentation/orders_list/order_item_list_tile.dart';
+import 'package:ecommerce_app/src/features/orders/presentation/orders_list/order_status_label.dart';
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:ecommerce_app/src/features/cart/domain/item.dart';
+import 'package:ecommerce_app/src/features/orders/domain/order.dart';
+import 'package:ecommerce_app/src/utils/currency_formatter.dart';
+import 'package:ecommerce_app/src/utils/date_formatter.dart';
+
+/// Shows all the details for a given order
+class OrderCard extends StatelessWidget {
+  const OrderCard({super.key, required this.order});
+  final Order order;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(width: 1, color: Colors.grey[400]!),
+        borderRadius: const BorderRadius.all(Radius.circular(Sizes.p8)),
+      ),
+      child: Column(
+        children: [
+          OrderHeader(order: order),
+          const Divider(height: 1, color: Colors.grey),
+          OrderItemsList(order: order),
+        ],
+      ),
+    );
+  }
+}
+
+/// Order header showing the following:
+/// - Total order amount
+/// - Order date
+class OrderHeader extends StatelessWidget {
+  const OrderHeader({super.key, required this.order});
+  final Order order;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Inject currency formatter
+    final totalFormatted = kCurrencyFormatter.format(order.total);
+    // TODO: Inject date formatter
+    final dateFormatted = kDateFormatter.format(order.orderDate);
+    return Container(
+      color: Colors.grey[200],
+      padding: const EdgeInsets.all(Sizes.p16),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Order placed'.hardcoded.toUpperCase(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  gapH4,
+                  Text(dateFormatted),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'Total'.hardcoded.toUpperCase(),
+                    textAlign: TextAlign.end,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  gapH4,
+                  Text(totalFormatted),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// List of items in the order
+class OrderItemsList extends StatelessWidget {
+  const OrderItemsList({super.key, required this.order});
+  final Order order;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(Sizes.p16),
+          child: OrderStatusLabel(order: order),
+        ),
+        for (var entry in order.items.entries)
+          OrderItemListTile(
+            item: Item(productId: entry.key, quantity: entry.value),
+          ),
+      ],
+    );
+  }
+}
---
+++ ecommerce_app/lib/src/features/products/presentation/home_app_bar/more_menu_button.dart
@@ -0,0 +1,66 @@
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:go_router/go_router.dart';
+
+enum PopupMenuOption {
+  signIn,
+  orders,
+  account,
+}
+
+class MoreMenuButton extends StatelessWidget {
+  const MoreMenuButton({super.key, this.user});
+  final AppUser? user;
+
+  // * Keys for testing using find.byKey()
+  static const signInKey = Key('menuSignIn');
+  static const ordersKey = Key('menuOrders');
+  static const accountKey = Key('menuAccount');
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      // three vertical dots icon (to reveal menu options)
+      icon: const Icon(Icons.more_vert),
+      itemBuilder: (_) {
+        // show all the options based on conditional logic
+        return user != null
+            ? <PopupMenuEntry<PopupMenuOption>>[
+                PopupMenuItem(
+                  key: ordersKey,
+                  value: PopupMenuOption.orders,
+                  child: Text('Orders'.hardcoded),
+                ),
+                PopupMenuItem(
+                  key: accountKey,
+                  value: PopupMenuOption.account,
+                  child: Text('Account'.hardcoded),
+                ),
+              ]
+            : <PopupMenuEntry<PopupMenuOption>>[
+                PopupMenuItem(
+                  key: signInKey,
+                  value: PopupMenuOption.signIn,
+                  child: Text('Sign In'.hardcoded),
+                ),
+              ];
+      },
+      onSelected: (option) {
+        // push to different routes based on selected option
+        switch (option) {
+          case PopupMenuOption.signIn:
+            context.pushNamed(AppRoute.signIn.name);
+            break;
+          case PopupMenuOption.orders:
+            context.pushNamed(AppRoute.orders.name);
+            break;
+          case PopupMenuOption.account:
+            context.pushNamed(AppRoute.account.name);
+            break;
+        }
+      },
+    );
+  }
+}
