eb684360188753ca46d52170f9e3416fb5bb890c
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-23 09:22:16 +0000

    Add fetchProductsList, watchProductsList, watchProduct

--- ecommerce_app/lib/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart
+++ ecommerce_app/lib/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
-import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/custom_image.dart';
@@ -31,8 +31,7 @@ class ShoppingCartItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Read from data source
-    final product =
-        kTestProducts.firstWhere((product) => product.id == item.productId);
+    final product = FakeProductsRepository.instance.getProduct(item.productId)!;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: Sizes.p8),
       child: Card(
