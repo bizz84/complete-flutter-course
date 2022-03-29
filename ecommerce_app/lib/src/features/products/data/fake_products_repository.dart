594c814dd2d303522a4032048efccf440edc59b4
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-29 16:09:33 +0100

    Use ConsumerWidget and Consumer to read productsRepositoryProvider

--- ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
+++ ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
@@ -3,9 +3,6 @@ import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeProductsRepository {
-  FakeProductsRepository._();
-  static FakeProductsRepository instance = FakeProductsRepository._();
-
   final List<Product> _products = kTestProducts;
 
   List<Product> getProductsList() {
@@ -31,5 +28,5 @@ class FakeProductsRepository {
 }
 
 final productsRepositoryProvider = Provider<FakeProductsRepository>((ref) {
-  return FakeProductsRepository.instance;
+  return FakeProductsRepository();
 });
