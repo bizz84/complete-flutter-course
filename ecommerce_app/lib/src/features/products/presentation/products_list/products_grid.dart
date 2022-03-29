7edddea5956916770ee236b4fc52dffd4a60a227
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-29 16:09:33 +0100

    Use ConsumerWidget and Consumer to read productsRepositoryProvider
    # Conflicts:
    #	ecommerce_app/lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart
    #	ecommerce_app/lib/src/features/products/presentation/products_list/products_grid.dart

--- ecommerce_app/lib/src/features/products/presentation/products_list/products_grid.dart
+++ ecommerce_app/lib/src/features/products/presentation/products_list/products_grid.dart
@@ -7,16 +7,17 @@ import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 /// A widget that displays the list of products that match the search query.
-class ProductsGrid extends StatelessWidget {
+class ProductsGrid extends ConsumerWidget {
   const ProductsGrid({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Read from data source
-    final products = FakeProductsRepository.instance.getProductsList();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productsRepository = ref.watch(productsRepositoryProvider);
+    final products = productsRepository.getProductsList();
     return products.isEmpty
         ? Center(
             child: Text(
