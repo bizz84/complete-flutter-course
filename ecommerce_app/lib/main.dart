6385bfa62d2349d741eb26c08aa0435bd6ad4a09
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-29 12:13:49 +0100

    Install and setup flutter_riverpod: 2.0.0-dev.5

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
@@ -13,7 +14,7 @@ void main() async {
     // turn off the # in the URLs on the web
     GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
     // * Entry point of the app
-    runApp(const MyApp());
+    runApp(const ProviderScope(child: MyApp()));
 
     // * This code will present some error UI if any uncaught exception happens
     FlutterError.onError = (FlutterErrorDetails details) {
