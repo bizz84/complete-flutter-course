393f961346f49714d223d749e5fbcf877115eb5b
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-29 16:09:33 +0100

    Use ConsumerWidget and Consumer to read productsRepositoryProvider
    # Conflicts:
    #	ecommerce_app/lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart
    #	ecommerce_app/lib/src/features/products/presentation/products_list/products_grid.dart

--- ecommerce_app/lib/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart
+++ ecommerce_app/lib/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart
@@ -10,10 +10,11 @@ import 'package:ecommerce_app/src/common_widgets/responsive_two_column_layout.da
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/features/cart/domain/item.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
 /// Shows a shopping cart item (or loading/error UI if needed)
-class ShoppingCartItem extends StatelessWidget {
+class ShoppingCartItem extends ConsumerWidget {
   const ShoppingCartItem({
     super.key,
     required this.item,
@@ -29,9 +30,9 @@ class ShoppingCartItem extends StatelessWidget {
   final bool isEditable;
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Read from data source
-    final product = FakeProductsRepository.instance.getProduct(item.productId)!;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productsRepository = ref.watch(productsRepositoryProvider);
+    final product = productsRepository.getProduct(item.productId)!;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: Sizes.p8),
       child: Card(
