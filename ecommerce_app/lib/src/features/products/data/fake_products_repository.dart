2a52a69810316d988e84553d7722231d5b0fcd6b
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-30 14:11:01 +0100

    Add StreamProvider.family

--- ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
+++ ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
@@ -42,3 +42,8 @@ final productsListFutureProvider = FutureProvider<List<Product>>((ref) {
   final productsRepository = ref.watch(productsRepositoryProvider);
   return productsRepository.fetchProductsList();
 });
+
+final productProvider = StreamProvider.family<Product?, String>((ref, id) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return productsRepository.watchProduct(id);
+});
