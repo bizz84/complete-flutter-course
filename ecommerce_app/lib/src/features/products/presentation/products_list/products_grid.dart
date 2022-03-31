ddc81a802b110bf56302d2be4d4722cf10b2808e
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-31 12:31:34 +0100

    Add AsyncValueWidget and use it in the product widgets
    # Conflicts:
    #	ecommerce_app/lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart

--- ecommerce_app/lib/src/features/products/presentation/products_list/products_grid.dart
+++ ecommerce_app/lib/src/features/products/presentation/products_list/products_grid.dart
@@ -1,7 +1,8 @@
 import 'dart:math';
 
-import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/features/products/presentation/products_list/product_card.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
@@ -18,7 +19,8 @@ class ProductsGrid extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final productsListValue = ref.watch(productsListStreamProvider);
-    return productsListValue.when(
+    return AsyncValueWidget<List<Product>>(
+      value: productsListValue,
       data: (products) => products.isEmpty
           ? Center(
               child: Text(
@@ -39,8 +41,6 @@ class ProductsGrid extends ConsumerWidget {
                 );
               },
             ),
-      error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
-      loading: () => const Center(child: CircularProgressIndicator()),
     );
   }
 }
