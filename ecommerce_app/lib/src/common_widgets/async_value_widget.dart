3be1229a5236b216ff9995095d00bf945aa1079d
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-31 12:31:34 +0100

    Add AsyncValueWidget and use it in the product widgets
    # Conflicts:
    #	ecommerce_app/lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart

---
+++ ecommerce_app/lib/src/common_widgets/async_value_widget.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AsyncValueWidget<T> extends StatelessWidget {
+  const AsyncValueWidget({super.key, required this.value, required this.data});
+  final AsyncValue<T> value;
+  final Widget Function(T) data;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
+      data: data,
+      error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
+      loading: () => const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
