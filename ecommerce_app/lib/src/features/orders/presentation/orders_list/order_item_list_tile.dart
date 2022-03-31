d7ba6110983d4f3cc964eb68c1c449fdba41ebf3
Andrea Bizzotto <codewithandrea@gmail.com>
2022-03-31 12:31:34 +0100

    Add AsyncValueWidget and use it in the product widgets
    # Conflicts:
    #	ecommerce_app/lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart

--- ecommerce_app/lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart
+++ ecommerce_app/lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/custom_image.dart';
@@ -13,32 +15,34 @@ class OrderItemListTile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final productsRepository = ref.watch(productsRepositoryProvider);
-    final product = productsRepository.getProduct(item.productId)!;
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: Sizes.p8),
-      child: Row(
-        children: [
-          Flexible(
-            flex: 1,
-            child: CustomImage(imageUrl: product.imageUrl),
-          ),
-          gapW8,
-          Flexible(
-            flex: 3,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(product.title),
-                gapH12,
-                Text(
-                  'Quantity: ${item.quantity}'.hardcoded,
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
+    final productValue = ref.watch(productProvider(item.productId));
+    return AsyncValueWidget<Product?>(
+      value: productValue,
+      data: (product) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: Sizes.p8),
+        child: Row(
+          children: [
+            Flexible(
+              flex: 1,
+              child: CustomImage(imageUrl: product!.imageUrl),
             ),
-          ),
-        ],
+            gapW8,
+            Flexible(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(product.title),
+                  gapH12,
+                  Text(
+                    'Quantity: ${item.quantity}'.hardcoded,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- ecommerce_app/lib/src/features/products/presentation/product_screen/product_screen.dart
+++ ecommerce_app/lib/src/features/products/presentation/product_screen/product_screen.dart
@@ -1,4 +1,4 @@
-import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/add_to_cart/add_to_cart_widget.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/home_app_bar.dart';
@@ -28,7 +28,8 @@ class ProductScreen extends StatelessWidget {
       body: Consumer(
         builder: (context, ref, _) {
           final productValue = ref.watch(productProvider(productId));
-          return productValue.when(
+          return AsyncValueWidget<Product?>(
+            value: productValue,
             data: (product) => product == null
                 ? EmptyPlaceholderWidget(
                     message: 'Product not found'.hardcoded,
@@ -42,8 +43,6 @@ class ProductScreen extends StatelessWidget {
                       ProductReviewsList(productId: productId),
                     ],
                   ),
-            error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
-            loading: () => const Center(child: CircularProgressIndicator()),
           );
         },
       ),
