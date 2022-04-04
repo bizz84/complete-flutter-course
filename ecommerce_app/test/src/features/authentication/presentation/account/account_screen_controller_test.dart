fa90ab97ff86d233fe7502ff01a3a81828e89d26
Andrea Bizzotto <codewithandrea@gmail.com>
2022-04-04 07:40:59 +0100

    Start adding AccountScreenController tests

---
+++ ecommerce_app/test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -0,0 +1,34 @@
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
+import 'package:ecommerce_app/src/utils/void_async_value.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../mocks.dart';
+
+void main() {
+  group('AccountScreenController', () {
+    test('signOut success', () async {
+      final authRepository = MockAuthRepository();
+      when(() => authRepository.signOut())
+          .thenAnswer((_) async => Future.value(null));
+      final controller = AccountScreenController(
+        authRepository: authRepository,
+      );
+      await controller.signOut();
+      verify(() => authRepository.signOut()).called(1);
+      expect(controller.debugState, const VoidAsyncValue.data(null));
+    });
+
+    test('signOut failure', () async {
+      final authRepository = MockAuthRepository();
+      final exception = Exception('Connection failed');
+      when(() => authRepository.signOut()).thenThrow(exception);
+      final controller = AccountScreenController(
+        authRepository: authRepository,
+      );
+      await controller.signOut();
+      verify(() => authRepository.signOut()).called(1);
+      expect(controller.debugState, VoidAsyncValue.error(exception));
+    });
+  });
+}
---
+++ ecommerce_app/test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('sign in', () {
+    test('Given submit called When can not submit Then returns Success(false)',
+        () async {
+      // TODO: Implement test
+    });
+
+    test(
+        'Given submit called When can submit And valid credentials Then returns Success(true)',
+        () async {
+      // TODO: Implement test
+    });
+
+    test(
+        'Given submit called When can submit And invalid credentials Then returns error',
+        () async {
+      // TODO: Implement test
+    });
+  });
+  group('register', () {
+    test('Given submit called When can not submit Then returns Success(false)',
+        () async {
+      // TODO: Implement test
+    });
+
+    test(
+        'Given submit called When can submit And valid credentials Then returns Success(true)',
+        () async {
+      // TODO: Implement test
+    });
+
+    test(
+        'Given submit called When can submit And invalid credentials Then returns error',
+        () async {
+      // TODO: Implement test
+    });
+  });
+}
