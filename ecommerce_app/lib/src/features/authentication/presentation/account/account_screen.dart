833bed5278893e303070910318ca487ab4c59f75
Andrea Bizzotto <codewithandrea@gmail.com>
2022-04-04 07:15:57 +0100

    Add AccountScreenController

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
@@ -1,8 +1,11 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_app/src/utils/async_value_ui.dart';
+import 'package:ecommerce_app/src/utils/void_async_value.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/action_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
@@ -13,27 +16,37 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class AccountScreen extends ConsumerWidget {
   const AccountScreen({Key? key}) : super(key: key);
 
+  Future<void> _handleLogout(BuildContext context, WidgetRef ref) async {
+    final logout = await showAlertDialog(
+      context: context,
+      title: 'Are you sure?'.hardcoded,
+      cancelActionText: 'Cancel'.hardcoded,
+      defaultActionText: 'Logout'.hardcoded,
+    );
+    if (logout == true) {
+      final didSignOut =
+          await ref.read(accountScreenControllerProvider.notifier).signOut();
+      if (didSignOut) {
+        Navigator.of(context).pop();
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<VoidAsyncValue>(
+      accountScreenControllerProvider,
+      (_, state) => state.showAlertDialogOnError(context),
+    );
+    final state = ref.watch(accountScreenControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text('Account'.hardcoded),
         actions: [
           ActionTextButton(
             text: 'Logout'.hardcoded,
-            onPressed: () async {
-              final logout = await showAlertDialog(
-                context: context,
-                title: 'Are you sure?'.hardcoded,
-                cancelActionText: 'Cancel'.hardcoded,
-                defaultActionText: 'Logout'.hardcoded,
-              );
-              if (logout == true) {
-                final authRepository = ref.read(authRepositoryProvider);
-                await authRepository.signOut();
-                Navigator.of(context).pop();
-              }
-            },
+            onPressed:
+                state.isLoading ? null : () => _handleLogout(context, ref),
           ),
         ],
       ),
---
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -0,0 +1,30 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/utils/void_async_value.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AccountScreenController extends StateNotifier<VoidAsyncValue> {
+  AccountScreenController({required this.authRepository})
+      : super(const VoidAsyncValue.data(null));
+  final FakeAuthRepository authRepository;
+
+  Future<bool> signOut() async {
+    try {
+      state = const VoidAsyncValue.loading();
+      await authRepository.signOut();
+      state = const VoidAsyncValue.data(null);
+      return true;
+    } catch (e) {
+      state = VoidAsyncValue.error(e);
+      return false;
+    }
+  }
+}
+
+final accountScreenControllerProvider =
+    StateNotifierProvider.autoDispose<AccountScreenController, VoidAsyncValue>(
+        (ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AccountScreenController(
+    authRepository: authRepository,
+  );
+});
