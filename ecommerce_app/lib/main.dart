57c8b3680428efc9c3745051056170a0cc4cacaa
Andrea Bizzotto <codewithandrea@gmail.com>
2022-04-06 12:17:31 +0100

    Add ProviderLogger

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/utils/provider_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -14,7 +15,10 @@ void main() async {
     // turn off the # in the URLs on the web
     GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
     // * Entry point of the app
-    runApp(const ProviderScope(child: MyApp()));
+    runApp(ProviderScope(
+      observers: [ProviderLogger()],
+      child: const MyApp(),
+    ));
 
     // * This code will present some error UI if any uncaught exception happens
     FlutterError.onError = (FlutterErrorDetails details) {
---
+++ ecommerce_app/lib/src/utils/provider_logger.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProviderLogger extends ProviderObserver {
+  @override
+  void didUpdateProvider(
+    ProviderBase provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    debugPrint('[${provider.name ?? provider.runtimeType}] value: $newValue');
+  }
+}
