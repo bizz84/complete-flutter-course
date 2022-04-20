251d306d3c654c19800341a8511a47605772d071
Andrea Bizzotto <codewithandrea@gmail.com>
2022-04-20 15:56:14 +0100

    Conditionally call Navigator.pop on sign out

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -6,13 +6,15 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
       : super(const AsyncValue<void>.data(null));
   final FakeAuthRepository authRepository;
 
-  Future<void> signOut() async {
+  Future<bool> signOut() async {
     try {
       state = const AsyncValue<void>.loading();
       await authRepository.signOut();
       state = const AsyncValue<void>.data(null);
+      return true;
     } catch (e) {
       state = AsyncValue<void>.error(e);
+      return false;
     }
   }
 }
