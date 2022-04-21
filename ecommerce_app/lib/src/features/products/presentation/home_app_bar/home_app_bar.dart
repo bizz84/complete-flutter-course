ff6e7050bfda00a404aa6febc37351614043e4a7
Andrea Bizzotto <codewithandrea@gmail.com>
2022-04-21 10:15:41 +0100

    Use authStateChangesProvider in AccountScreen and HomeAppBar
    # Conflicts:
    #	ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
    #	ecommerce_app/lib/src/features/products/presentation/home_app_bar/home_app_bar.dart
    
    # Conflicts:
    #	ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
    #	ecommerce_app/lib/src/features/products/presentation/home_app_bar/home_app_bar.dart

--- ecommerce_app/lib/src/features/products/presentation/home_app_bar/home_app_bar.dart
+++ ecommerce_app/lib/src/features/products/presentation/home_app_bar/home_app_bar.dart
@@ -1,11 +1,12 @@
 import 'package:ecommerce_app/src/constants/breakpoints.dart';
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/more_menu_button.dart';
 import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/shopping_cart_icon.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
-import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/action_text_button.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 /// Custom [AppBar] widget that is reused by the [ProductsListScreen] and
@@ -14,13 +15,12 @@ import 'package:go_router/go_router.dart';
 /// - [ShoppingCartIcon]
 /// - Orders button
 /// - Account or Sign-in button
-class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+class HomeAppBar extends ConsumerWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: get user from auth repository
-    const user = AppUser(uid: '123', email: 'test@test.com');
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(authStateChangesProvider).value;
     // * This widget is responsive.
     // * On large screen sizes, it shows all the actions in the app bar.
     // * On small screen sizes, it shows only the shopping cart icon and a
@@ -32,8 +32,8 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
     if (screenWidth < Breakpoint.tablet) {
       return AppBar(
         title: Text('My Shop'.hardcoded),
-        actions: const [
-          ShoppingCartIcon(),
+        actions: [
+          const ShoppingCartIcon(),
           MoreMenuButton(user: user),
         ],
       );
