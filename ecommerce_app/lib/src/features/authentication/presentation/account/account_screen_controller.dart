bf8b808f0ab0231f90b445650e1f2a5939052b91
Andrea Bizzotto <codewithandrea@gmail.com>
2022-04-21 08:45:18 +0100

    Use AsyncValue.guard()
    # Conflicts:
    #	ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -7,15 +7,9 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   final FakeAuthRepository authRepository;
 
   Future<bool> signOut() async {
-    try {
-      state = const AsyncValue<void>.loading();
-      await authRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-      return true;
-    } catch (e, st) {
-      state = AsyncValue<void>.error(e, st);
-      return false;
-    }
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+    return state.hasError == false;
   }
 }
 
