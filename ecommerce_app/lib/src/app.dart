cd9c2cfd7c4d5faf663b490d4097ec43b3ea4a5e
Andrea Bizzotto <codewithandrea@gmail.com>
2022-04-22 12:30:39 +0100

    Update GoRouter code to use Provider, add redirect
    # Conflicts:
    #	ecommerce_app/lib/src/app.dart
    
    # Conflicts:
    #	ecommerce_app/lib/src/routing/app_router.dart
    
    # Conflicts:
    #	ecommerce_app/lib/src/routing/app_router.dart

--- ecommerce_app/lib/src/app.dart
+++ ecommerce_app/lib/src/app.dart
@@ -1,12 +1,14 @@
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
     return MaterialApp.router(
       routerConfig: goRouter,
       debugShowCheckedModeBanner: false,
