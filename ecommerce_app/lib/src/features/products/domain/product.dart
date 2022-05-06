15ab97483c510eb6794c55e489f818961ecd6d73
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-06 14:05:22 +0100

    Add first test + product equality methods

--- ecommerce_app/lib/src/features/products/domain/product.dart
+++ ecommerce_app/lib/src/features/products/domain/product.dart
@@ -23,4 +23,36 @@ class Product {
   final int availableQuantity;
   final double avgRating;
   final int numRatings;
+
+  @override
+  String toString() {
+    return 'Product(id: $id, imageUrl: $imageUrl, title: $title, description: $description, price: $price, availableQuantity: $availableQuantity, avgRating: $avgRating, numRatings: $numRatings)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Product &&
+        other.id == id &&
+        other.imageUrl == imageUrl &&
+        other.title == title &&
+        other.description == description &&
+        other.price == price &&
+        other.availableQuantity == availableQuantity &&
+        other.avgRating == avgRating &&
+        other.numRatings == numRatings;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        imageUrl.hashCode ^
+        title.hashCode ^
+        description.hashCode ^
+        price.hashCode ^
+        availableQuantity.hashCode ^
+        avgRating.hashCode ^
+        numRatings.hashCode;
+  }
 }
