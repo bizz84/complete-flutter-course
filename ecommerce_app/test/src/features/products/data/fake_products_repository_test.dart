d8ace8d21a827c9ccf949f9357a52ed1aad29f7c
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-06 14:05:22 +0100

    Add first test + product equality methods

--- ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
+++ ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -1,7 +1,13 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('fake products repository ...', (tester) async {
-    // TODO: Implement test
+  test('getProductsList returns global list', () {
+    final productsRepository = FakeProductsRepository();
+    expect(
+      productsRepository.getProductsList(),
+      kTestProducts,
+    );
   });
 }
