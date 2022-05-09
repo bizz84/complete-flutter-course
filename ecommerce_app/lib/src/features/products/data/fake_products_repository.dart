65dca45419a568e76f21abde6a03639cb8bfc5ec
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-09 10:07:06 +0100

    Implement private _getProduct method + update test

--- ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
+++ ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
@@ -10,7 +10,7 @@ class FakeProductsRepository {
   }
 
   Product? getProduct(String id) {
-    return _products.firstWhere((product) => product.id == id);
+    return _getProduct(_products, id);
   }
 
   Future<List<Product>> fetchProductsList() async {
@@ -24,8 +24,15 @@ class FakeProductsRepository {
   }
 
   Stream<Product?> watchProduct(String id) {
-    return watchProductsList()
-        .map((products) => products.firstWhere((product) => product.id == id));
+    return watchProductsList().map((products) => _getProduct(products, id));
+  }
+
+  static Product? _getProduct(List<Product> products, String id) {
+    try {
+      return products.firstWhere((product) => product.id == id);
+    } catch (e) {
+      return null;
+    }
   }
 }
 
