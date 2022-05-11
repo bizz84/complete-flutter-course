993b45d8f16fd9563ffe3351bd5a341e3100e664
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-11 20:53:16 +0100

    Complete EmailPasswordSignInController tests

--- ecommerce_app/test/src/features/authentication/presentation/account/account_screen_controller_test.dart
+++ ecommerce_app/test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -1,10 +1,9 @@
-import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockAuthRepository extends Mock implements FakeAuthRepository {}
+import '../../../../mocks.dart';
 
 void main() {
   group('AccountScreenController', () {
--- ecommerce_app/test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
+++ ecommerce_app/test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
@@ -1,11 +1,10 @@
-import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_controller.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockAuthRepository extends Mock implements FakeAuthRepository {}
+import '../../../../mocks.dart';
 
 void main() {
   const testEmail = 'test@test.com';
@@ -71,6 +70,110 @@ void main() {
         }),
       );
     });
+    test('''
+    Given formType is register
+    When createUserWithEmailAndPassword succeeds
+    Then return true
+    And state is AsyncData
+    ''', () async {
+      // setup
+      final authRepository = MockAuthRepository();
+      when(() => authRepository.createUserWithEmailAndPassword(
+            testEmail,
+            testPassword,
+          )).thenAnswer((_) => Future.value());
+      final controller = EmailPasswordSignInController(
+        authRepository: authRepository,
+        formType: EmailPasswordSignInFormType.register,
+      );
+      // run
+      final result = await controller.submit(testEmail, testPassword);
+      // verify
+      expect(result, true);
+      expect(
+        controller.debugState,
+        EmailPasswordSignInState(
+          formType: EmailPasswordSignInFormType.register,
+          value: const AsyncData<void>(null),
+        ),
+      );
+    });
+    test('''
+    Given formType is register
+    When createUserWithEmailAndPassword fails
+    Then return false
+    And state is AsyncError
+    ''', () async {
+      // setup
+      final authRepository = MockAuthRepository();
+      final exception = Exception('Connection failed');
+      when(() => authRepository.createUserWithEmailAndPassword(
+            testEmail,
+            testPassword,
+          )).thenThrow(exception);
+      final controller = EmailPasswordSignInController(
+        authRepository: authRepository,
+        formType: EmailPasswordSignInFormType.register,
+      );
+      // run
+      final result = await controller.submit(testEmail, testPassword);
+      // verify
+      expect(result, false);
+      expect(
+        controller.debugState,
+        // using predicate since we can't match the stack trace
+        predicate<EmailPasswordSignInState>((state) {
+          expect(state.formType, EmailPasswordSignInFormType.register);
+          expect(state.value.hasError, true);
+          return true;
+        }),
+      );
+    });
   });
-  group('updateFormType', () {});
+  group('updateFormType', () {
+    test('''
+    Given formType is signIn
+    When called with register
+    Then state.formType is register
+    ''', () {
+      // setup
+      final authRepository = MockAuthRepository();
+      final controller = EmailPasswordSignInController(
+        authRepository: authRepository,
+        formType: EmailPasswordSignInFormType.signIn,
+      );
+      // run
+      controller.updateFormType(EmailPasswordSignInFormType.register);
+      // verify
+      expect(
+        controller.debugState,
+        EmailPasswordSignInState(
+          formType: EmailPasswordSignInFormType.register,
+          value: const AsyncData<void>(null),
+        ),
+      );
+    });
+    test('''
+    Given formType is register
+    When called with signIn
+    Then state.formType is signIn
+    ''', () {
+      // setup
+      final authRepository = MockAuthRepository();
+      final controller = EmailPasswordSignInController(
+        authRepository: authRepository,
+        formType: EmailPasswordSignInFormType.register,
+      );
+      // run
+      controller.updateFormType(EmailPasswordSignInFormType.signIn);
+      // verify
+      expect(
+        controller.debugState,
+        EmailPasswordSignInState(
+          formType: EmailPasswordSignInFormType.signIn,
+          value: const AsyncData<void>(null),
+        ),
+      );
+    });
+  });
 }
