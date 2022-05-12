7c4ec5f07f4b7b62191bde2d1067573a9760f435
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-12 06:49:13 +0100

    Move WidgetsFlutterBinding.ensureInitialized() inside runZoneGuarded()
    https://docs.flutter.dev/testing/errors

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -5,10 +5,10 @@ import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   // * For more info on error handling, see:
   // * https://docs.flutter.dev/testing/errors
   await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
     // * Entry point of the app
     runApp(const MyApp());
 
