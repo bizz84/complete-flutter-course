a8bbe5ce658a57086f1c8de8bf04d437f6e5fc36
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-23 14:33:37 +0100

    Complete EmailPasswordSignInController tests

---
+++ ecommerce_app/test/src/mocks.dart
@@ -0,0 +1,4 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements FakeAuthRepository {}
