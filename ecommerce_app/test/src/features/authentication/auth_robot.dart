cddf2bd91b60333490d538454e95424e5bdedb1a
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-24 15:42:52 +0100

    Implement AuthRobot

---
+++ ecommerce_app/test/src/features/authentication/auth_robot.dart
@@ -0,0 +1,43 @@
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class AuthRobot {
+  AuthRobot(this.tester);
+  final WidgetTester tester;
+
+  Future<void> pumpAccountScreen() async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: AccountScreen(),
+        ),
+      ),
+    );
+  }
+
+  Future<void> tapLogoutButton() async {
+    final logoutButton = find.text('Logout');
+    expect(logoutButton, findsOneWidget);
+    await tester.tap(logoutButton);
+    await tester.pump();
+  }
+
+  void expectLogoutDialogFound() {
+    final dialogTitle = find.text('Are you sure?');
+    expect(dialogTitle, findsOneWidget);
+  }
+
+  Future<void> tapCancelButton() async {
+    final cancelButton = find.text('Cancel');
+    expect(cancelButton, findsOneWidget);
+    await tester.tap(cancelButton);
+    await tester.pump();
+  }
+
+  void expectLogoutDialogNotFound() {
+    final dialogTitle = find.text('Are you sure?');
+    expect(dialogTitle, findsNothing);
+  }
+}
