e3906d099599ab51ce9f54be3263e2d79977ebfd
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-24 11:27:41 +0100

    Add consistent behavior for barrierDismissible inside showAlertDialog()

--- ecommerce_app/lib/src/common_widgets/alert_dialogs.dart
+++ ecommerce_app/lib/src/common_widgets/alert_dialogs.dart
@@ -16,6 +16,7 @@ Future<bool?> showAlertDialog({
   if (kIsWeb || !Platform.isIOS) {
     return showDialog(
       context: context,
+      barrierDismissible: cancelActionText != null,
       builder: (context) => AlertDialog(
         title: Text(title),
         content: content != null ? Text(content) : null,
@@ -35,6 +36,7 @@ Future<bool?> showAlertDialog({
   }
   return showCupertinoDialog(
     context: context,
+    barrierDismissible: cancelActionText != null,
     builder: (context) => CupertinoAlertDialog(
       title: Text(title),
       content: content != null ? Text(content) : null,
