aeb312936722e9b950fd909275e351d7694467f9
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-25 15:02:55 +0100

    More email & password sign in tests

--- ecommerce_app/test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
+++ ecommerce_app/test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
@@ -173,7 +173,7 @@ void main() {
       controller.updateFormType(EmailPasswordSignInFormType.register);
       // verify
       expect(
-        controller.debugState,
+        controller.state,
         EmailPasswordSignInState(
           formType: EmailPasswordSignInFormType.register,
           value: const AsyncData<void>(null),
@@ -196,7 +196,7 @@ void main() {
       controller.updateFormType(EmailPasswordSignInFormType.signIn);
       // verify
       expect(
-        controller.debugState,
+        controller.state,
         EmailPasswordSignInState(
           formType: EmailPasswordSignInFormType.signIn,
           value: const AsyncData<void>(null),
