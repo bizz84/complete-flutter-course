a89a5ade41a74fb13fc9cfd5dd8d8287101eba3b
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-25 11:22:59 +0100

    Add "Confirm logout, failure" test

--- ecommerce_app/test/src/features/authentication/presentation/account/account_screen_test.dart
+++ ecommerce_app/test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -1,5 +1,8 @@
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
+import '../../../../mocks.dart';
 import '../../auth_robot.dart';
 
 void main() {
@@ -11,12 +14,29 @@ void main() {
     await r.tapCancelButton();
     r.expectLogoutDialogNotFound();
   });
-  testWidgets('Confirm logout success', (tester) async {
+  testWidgets('Confirm logout, success', (tester) async {
     final r = AuthRobot(tester);
     await r.pumpAccountScreen();
     await r.tapLogoutButton();
     r.expectLogoutDialogFound();
     await r.tapDialogLogoutButton();
     r.expectLogoutDialogNotFound();
+    r.expectErrorAlertNotFound();
+  });
+  testWidgets('Confirm logout, failure', (tester) async {
+    final r = AuthRobot(tester);
+    final authRepository = MockAuthRepository();
+    final exception = Exception('Connection Failed');
+    when(authRepository.signOut).thenThrow(exception);
+    when(authRepository.authStateChanges).thenAnswer(
+      (_) => Stream.value(
+        const AppUser(uid: '123', email: 'test@test.com'),
+      ),
+    );
+    await r.pumpAccountScreen(authRepository: authRepository);
+    await r.tapLogoutButton();
+    r.expectLogoutDialogFound();
+    await r.tapDialogLogoutButton();
+    r.expectErrorAlertFound();
   });
 }
