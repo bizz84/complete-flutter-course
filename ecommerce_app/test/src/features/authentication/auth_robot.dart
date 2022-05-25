f8bee5da449bbfb5c8726910ea39dc667557bb6c
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-25 11:22:59 +0100

    Add "Confirm logout, failure" test

--- ecommerce_app/test/src/features/authentication/auth_robot.dart
+++ ecommerce_app/test/src/features/authentication/auth_robot.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,10 +9,16 @@ class AuthRobot {
   AuthRobot(this.tester);
   final WidgetTester tester;
 
-  Future<void> pumpAccountScreen() async {
+  Future<void> pumpAccountScreen({FakeAuthRepository? authRepository}) async {
     await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(
+      ProviderScope(
+        overrides: [
+          if (authRepository != null)
+            authRepositoryProvider.overrideWithValue(
+              authRepository,
+            )
+        ],
+        child: const MaterialApp(
           home: AccountScreen(),
         ),
       ),
@@ -47,5 +54,15 @@ class AuthRobot {
     expect(logoutButton, findsOneWidget);
     await tester.tap(logoutButton);
     await tester.pump();
+  }
+
+  void expectErrorAlertFound() {
+    final finder = find.text('Error');
+    expect(finder, findsOneWidget);
+  }
+
+  void expectErrorAlertNotFound() {
+    final finder = find.text('Error');
+    expect(finder, findsNothing);
   }
 }
