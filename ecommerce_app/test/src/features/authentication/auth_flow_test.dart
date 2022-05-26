5f2b9d18b1087b8915be16bfbd5e7aa027011219
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-26 10:02:43 +0100

    Start implementing Robot with auth flow tests + set optionURLReflectsImperativeAPIs in tests

---
+++ ecommerce_app/test/src/features/authentication/auth_flow_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../robot.dart';
+
+void main() {
+  testWidgets('Sign in and sign out flow', (tester) async {
+    final r = Robot(tester);
+    await r.pumpMyApp();
+    r.expectFindAllProductCards();
+  });
+}
