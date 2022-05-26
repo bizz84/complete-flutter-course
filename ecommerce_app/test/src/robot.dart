65dc65b62f1d001bba630af9cb1f3feed5013905
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-26 11:48:45 +0100

    Completed auth flow test

--- ecommerce_app/test/src/robot.dart
+++ ecommerce_app/test/src/robot.dart
@@ -2,13 +2,17 @@ import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/more_menu_button.dart';
 import 'package:ecommerce_app/src/features/products/presentation/products_list/product_card.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'features/authentication/auth_robot.dart';
+
 class Robot {
-  Robot(this.tester);
+  Robot(this.tester) : auth = AuthRobot(tester);
   final WidgetTester tester;
+  final AuthRobot auth;
 
   Future<void> pumpMyApp() async {
     // Override repositories
@@ -29,5 +33,17 @@ class Robot {
   void expectFindAllProductCards() {
     final finder = find.byType(ProductCard);
     expect(finder, findsNWidgets(kTestProducts.length));
+  }
+
+  Future<void> openPopupMenu() async {
+    final finder = find.byType(MoreMenuButton);
+    final matches = finder.evaluate();
+    // if an item is found, it means that we're running
+    // on a small window and can tap to reveal the menu
+    if (matches.isNotEmpty) {
+      await tester.tap(finder);
+      await tester.pumpAndSettle();
+    }
+    // else no-op, as the items are already visible
   }
 }
