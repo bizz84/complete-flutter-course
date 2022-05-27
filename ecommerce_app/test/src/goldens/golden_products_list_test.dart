08ab0bf12f66bed5f2a2ba28547c765ae9d9960c
Andrea Bizzotto <codewithandrea@gmail.com>
2022-05-27 10:33:18 +0100

    Complete golden image tests for the products list

--- ecommerce_app/test/src/goldens/golden_products_list_test.dart
+++ ecommerce_app/test/src/goldens/golden_products_list_test.dart
@@ -1,20 +1,34 @@
+import 'dart:ui';
+
 import 'package:ecommerce_app/src/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../robot.dart';
 
 void main() {
-  testWidgets('Golden - products list', (tester) async {
-    final r = Robot(tester);
-    await r.golden.loadRobotoFont();
-    await r.golden.loadMaterialIconFont();
-    await r.pumpMyApp();
-    await r.golden.precacheImages();
-    await expectLater(
-      find.byType(MyApp),
-      matchesGoldenFile(
-        'products_list.png',
-      ),
-    );
+  final sizeVariant = ValueVariant<Size>({
+    const Size(300, 600),
+    const Size(600, 800),
+    const Size(1000, 1000),
   });
+  testWidgets(
+    'Golden - products list',
+    (tester) async {
+      final r = Robot(tester);
+      final currentSize = sizeVariant.currentValue!;
+      await r.golden.setSurfaceSize(currentSize);
+      await r.golden.loadRobotoFont();
+      await r.golden.loadMaterialIconFont();
+      await r.pumpMyApp();
+      await r.golden.precacheImages();
+      await expectLater(
+        find.byType(MyApp),
+        matchesGoldenFile(
+          'products_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png',
+        ),
+      );
+    },
+    variant: sizeVariant,
+    tags: ['golden'],
+  );
 }
