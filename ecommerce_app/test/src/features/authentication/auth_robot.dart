7bc2bf009fcaf0d5959cf759c22dc8d049087520
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-07 09:29:34 +0100

    Add additional tests for EmailPasswordSignInContents, additional tester.pump() in signInWithEmailAndPassword helper

--- ecommerce_app/test/src/features/authentication/auth_robot.dart
+++ ecommerce_app/test/src/features/authentication/auth_robot.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/common_widgets/custom_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
@@ -49,6 +50,13 @@ class AuthRobot {
     await tester.pumpAndSettle();
   }
 
+  Future<void> tapFormToggleButton() async {
+    final toggleButton = find.byType(CustomTextButton);
+    expect(toggleButton, findsOneWidget);
+    await tester.tap(toggleButton);
+    await tester.pumpAndSettle();
+  }
+
   Future<void> enterEmail(String email) async {
     final emailField = find.byKey(EmailPasswordSignInScreen.emailKey);
     expect(emailField, findsOneWidget);
@@ -61,8 +69,19 @@ class AuthRobot {
     await tester.enterText(passwordField, password);
   }
 
+  void expectCreateAccountButtonFound() {
+    final dialogTitle = find.text('Create an account');
+    expect(dialogTitle, findsOneWidget);
+  }
+
+  void expectCreateAccountButtonNotFound() {
+    final dialogTitle = find.text('Create an account');
+    expect(dialogTitle, findsNothing);
+  }
+
   Future<void> signInWithEmailAndPassword() async {
     await enterEmail('test@test.com');
+    await tester.pump();
     await enterPassword('test1234');
     await tapEmailAndPasswordSubmitButton();
   }
