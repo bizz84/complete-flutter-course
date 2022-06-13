eb32175f6887f35cf163dec64d6aef5bb07d38c0
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-13 10:36:19 +0100

    Update MutableCart extension and tests

--- ecommerce_app/test/src/features/cart/domain/mutable_cart_test.dart
+++ ecommerce_app/test/src/features/cart/domain/mutable_cart_test.dart
@@ -4,6 +4,29 @@ import 'package:ecommerce_app/src/features/cart/domain/item.dart';
 import 'package:ecommerce_app/src/features/cart/domain/mutable_cart.dart';
 
 void main() {
+  group('set item', () {
+    test('empty cart - set item with quantity', () {
+      final cart =
+          const Cart().setItem(const Item(productId: '1', quantity: 2));
+      expect(cart.items, {'1': 2});
+    });
+    test('cart with same item - override quantity', () {
+      final cart = const Cart()
+          .addItem(const Item(productId: '1', quantity: 1))
+          .setItem(const Item(productId: '1', quantity: 2));
+      expect(cart.items, {'1': 2});
+    });
+    test('cart with different item - set item with quantity', () {
+      final cart = const Cart()
+          .addItem(const Item(productId: '2', quantity: 1))
+          .setItem(const Item(productId: '1', quantity: 2));
+      expect(cart.items, {
+        '2': 1,
+        '1': 2,
+      });
+    });
+  });
+
   group('add item', () {
     test('empty cart - add item', () {
       final cart =
@@ -26,6 +49,44 @@ void main() {
       expect(cart.items, {'1': 2});
     });
   });
+
+  group('add items', () {
+    test('empty cart - add two items', () {
+      final cart = const Cart().addItems([
+        const Item(productId: '1', quantity: 1),
+        const Item(productId: '2', quantity: 1),
+      ]);
+      expect(cart.items, {
+        '1': 1,
+        '2': 1,
+      });
+    });
+    test('cart with one item - add two items of which one matching', () {
+      final cart = const Cart()
+          .addItem(const Item(productId: '1', quantity: 1))
+          .addItems([
+        const Item(productId: '1', quantity: 1),
+        const Item(productId: '2', quantity: 1),
+      ]);
+      expect(cart.items, {
+        '1': 2,
+        '2': 1,
+      });
+    });
+    test('cart with one item - add two new items', () {
+      final cart = const Cart()
+          .addItem(const Item(productId: '1', quantity: 1))
+          .addItems([
+        const Item(productId: '2', quantity: 1),
+        const Item(productId: '3', quantity: 1),
+      ]);
+      expect(cart.items, {
+        '1': 1,
+        '2': 1,
+        '3': 1,
+      });
+    });
+  });
   group('remove item', () {
     test('empty cart - remove item', () {
       final cart = const Cart().removeItemById('1');
@@ -41,36 +102,6 @@ void main() {
       final cart = const Cart()
           .addItem(const Item(productId: '2', quantity: 1))
           .removeItemById('1');
-      expect(cart.items, {'2': 1});
-    });
-  });
-
-  group('remove all items', () {
-    test('some items - all', () {
-      final cart = const Cart()
-          .addItem(const Item(productId: '1', quantity: 1))
-          .addItem(const Item(productId: '2', quantity: 2))
-          .clear();
-      expect(cart.items, {});
-    });
-  });
-
-  group('update quantity', () {
-    test('empty cart - update quantity', () {
-      final cart = const Cart()
-          .updateItemIfExists(const Item(productId: '1', quantity: 2));
-      expect(cart.items, {});
-    });
-    test('empty cart - update quantity matching item', () {
-      final cart = const Cart()
-          .addItem(const Item(productId: '1', quantity: 1))
-          .updateItemIfExists(const Item(productId: '1', quantity: 2));
-      expect(cart.items, {'1': 2});
-    });
-    test('empty cart - update quantity non-matching item', () {
-      final cart = const Cart()
-          .addItem(const Item(productId: '2', quantity: 1))
-          .updateItemIfExists(const Item(productId: '1', quantity: 2));
       expect(cart.items, {'2': 1});
     });
   });
