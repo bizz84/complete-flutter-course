c4082be88404cecb6d84a4e3750a8570575a856a
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-13 10:55:57 +0100

    Starter project for section 8 (#14)
    * Update MutableCart extension, add corresponding tests
    
    * Use dateFormatterProvider and currencyFormatterProvider rather than global formatters
    
    * Set addDelay to false for faster loading
    
    * Add FakeLocalCartRepository and FakeRemoteCartRepository
    
    * Cleanup LocalCartRepository, RemoteCartRepository
    
    * Update MutableCart extension and tests
    
    * Updated to latest package versions
    
    * Improved documentation

--- ecommerce_app/lib/src/utils/currency_formatter.dart
+++ ecommerce_app/lib/src/utils/currency_formatter.dart
@@ -1,4 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
-/// Currency formatter to be used in the app.
-final kCurrencyFormatter = NumberFormat.simpleCurrency();
+final currencyFormatterProvider = Provider<NumberFormat>((ref) {
+  /// Currency formatter to be used in the app.
+  /// * en_US is hardcoded to ensure all prices show with a dollar sign ($)
+  /// * This may or may not be what you want in your own apps.
+  return NumberFormat.simpleCurrency(locale: "en_US");
+});
