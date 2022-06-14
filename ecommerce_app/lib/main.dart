2c14e7148d368584c540f24228d1e5de0069c5fd
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-14 15:08:53 +0100

    Install sembast 3.2.0 and create SembastCartRepository class

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/sembast_cart_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,8 +15,14 @@ void main() async {
     WidgetsFlutterBinding.ensureInitialized();
     // turn off the # in the URLs on the web
     GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+    final localCartRepository = await SembastCartRepository.makeDefault();
     // * Entry point of the app
-    runApp(const ProviderScope(child: MyApp()));
+    runApp(ProviderScope(
+      overrides: [
+        localCartRepositoryProvider.overrideWithValue(localCartRepository),
+      ],
+      child: const MyApp(),
+    ));
 
     // * This code will present some error UI if any uncaught exception happens
     FlutterError.onError = (FlutterErrorDetails details) {
