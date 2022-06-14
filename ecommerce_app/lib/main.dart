f0286bdd84a18da6bcdfaa948e2819c931ed2052
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-14 15:08:53 +0100

    Install sembast 3.7.5 and create SembastCartRepository class
    # Conflicts:
    #	ecommerce_app/macos/Podfile.lock
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/lib/main.dart
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/macos/Podfile.lock
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/ios/Podfile.lock
    #	ecommerce_app/macos/Runner.xcodeproj/project.pbxproj
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/ios/Podfile.lock
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/sembast_cart_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +15,14 @@ void main() async {
   // * Register error handlers. For more info, see:
   // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
+  final localCartRepository = await SembastCartRepository.makeDefault();
   // * Entry point of the app
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(ProviderScope(
+    overrides: [
+      localCartRepositoryProvider.overrideWithValue(localCartRepository),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 void registerErrorHandlers() {
