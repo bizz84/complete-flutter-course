a4d0f41374d21e73050d65dba51d286851963220
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-27 15:17:42 +0100

    Add cartProvider and use it in the ShoppingCartScreen

--- ecommerce_app/lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen.dart
+++ ecommerce_app/lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen.dart
@@ -1,10 +1,13 @@
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_items_builder.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
-import 'package:ecommerce_app/src/features/cart/domain/item.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 /// Shopping cart screen showing the items in the cart (with editable
@@ -15,35 +18,28 @@ class ShoppingCartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: error handling
-    // TODO: Read from data source
-    const cartItemsList = [
-      Item(
-        productId: '1',
-        quantity: 1,
-      ),
-      Item(
-        productId: '2',
-        quantity: 2,
-      ),
-      Item(
-        productId: '3',
-        quantity: 3,
-      ),
-    ];
     return Scaffold(
       appBar: AppBar(
         title: Text('Shopping Cart'.hardcoded),
       ),
-      body: ShoppingCartItemsBuilder(
-        items: cartItemsList,
-        itemBuilder: (_, item, index) => ShoppingCartItem(
-          item: item,
-          itemIndex: index,
-        ),
-        ctaBuilder: (_) => PrimaryButton(
-          text: 'Checkout'.hardcoded,
-          onPressed: () => context.goNamed(AppRoute.checkout.name),
-        ),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final cartValue = ref.watch(cartProvider);
+          return AsyncValueWidget<Cart>(
+            value: cartValue,
+            data: (cart) => ShoppingCartItemsBuilder(
+              items: cart.toItemsList(),
+              itemBuilder: (_, item, index) => ShoppingCartItem(
+                item: item,
+                itemIndex: index,
+              ),
+              ctaBuilder: (_) => PrimaryButton(
+                text: 'Checkout'.hardcoded,
+                onPressed: () => context.goNamed(AppRoute.checkout.name),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
