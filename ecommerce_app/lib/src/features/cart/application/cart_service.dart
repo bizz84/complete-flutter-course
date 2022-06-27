1d083a63dce0a8a1a47b66f003fc6cf151c0ea53
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-27 15:17:42 +0100

    Add cartProvider and use it in the ShoppingCartScreen

--- ecommerce_app/lib/src/features/cart/application/cart_service.dart
+++ ecommerce_app/lib/src/features/cart/application/cart_service.dart
@@ -59,3 +59,12 @@ class CartService {
 final cartServiceProvider = Provider<CartService>((ref) {
   return CartService(ref);
 });
+
+final cartProvider = StreamProvider<Cart>((ref) {
+  final user = ref.watch(authStateChangesProvider).value;
+  if (user != null) {
+    return ref.watch(remoteCartRepositoryProvider).watchCart(user.uid);
+  } else {
+    return ref.watch(localCartRepositoryProvider).watchCart();
+  }
+});
