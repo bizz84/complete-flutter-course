6d6d183f6ed5b096a2d18ba39b86568a8c87daf8
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-27 15:17:42 +0100

    Add cartProvider and use it in the ShoppingCartScreen

--- ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -42,7 +42,7 @@ final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
   return auth;
 });
 
-final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
+final authStateChangesProvider = StreamProvider<AppUser?>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return authRepository.authStateChanges();
 });
