e87dbc7dd00bd3da0c8099db82a278442beaa6e1
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-27 12:03:42 +0100

    autoDispose the addToCartControllerProvider + tests

--- ecommerce_app/test/src/mocks.dart
+++ ecommerce_app/test/src/mocks.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
 import 'package:mocktail/mocktail.dart';
@@ -8,3 +9,5 @@ class MockAuthRepository extends Mock implements FakeAuthRepository {}
 class MockRemoteCartRepository extends Mock implements RemoteCartRepository {}
 
 class MockLocalCartRepository extends Mock implements LocalCartRepository {}
+
+class MockCartService extends Mock implements CartService {}
