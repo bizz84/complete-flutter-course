1ade38f8b10ff4fff4b9421540d7564e0cffcafb
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-28 14:33:51 +0100

    Add itemAvailableQuantityProvider and use it in the AddToCartWidget + updated tests

--- ecommerce_app/lib/src/features/cart/application/cart_service.dart
+++ ecommerce_app/lib/src/features/cart/application/cart_service.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
@@ -90,5 +92,18 @@ final cartTotalProvider = Provider.autoDispose<double>((ref) {
     return total;
   } else {
     return 0.0;
+  }
+});
+
+final itemAvailableQuantityProvider =
+    Provider.autoDispose.family<int, Product>((ref, product) {
+  final cart = ref.watch(cartProvider).value;
+  if (cart != null) {
+    // get the current quantity for the given product in the cart
+    final quantity = cart.items[product.id] ?? 0;
+    // subtract it from the product available quantity
+    return max(0, product.availableQuantity - quantity);
+  } else {
+    return product.availableQuantity;
   }
 });
