fcd763fb0bf8cf3dd2fbed73818113a53c35261a
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-28 10:36:40 +0100

    Update ShoppingCartScreen and EditOrRemoveItemWidget

--- ecommerce_app/lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen.dart
+++ ecommerce_app/lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen.dart
@@ -3,8 +3,10 @@ import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
 import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_items_builder.dart';
+import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_screen_controller.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
+import 'package:ecommerce_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,12 +14,16 @@ import 'package:go_router/go_router.dart';
 
 /// Shopping cart screen showing the items in the cart (with editable
 /// quantities) and a button to checkout.
-class ShoppingCartScreen extends StatelessWidget {
+class ShoppingCartScreen extends ConsumerWidget {
   const ShoppingCartScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: error handling
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AsyncValue<void>>(
+      shoppingCartScreenControllerProvider,
+      (_, state) => state.showAlertDialogOnError(context),
+    );
+    final state = ref.watch(shoppingCartScreenControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text('Shopping Cart'.hardcoded),
@@ -35,6 +41,7 @@ class ShoppingCartScreen extends StatelessWidget {
               ),
               ctaBuilder: (_) => PrimaryButton(
                 text: 'Checkout'.hardcoded,
+                isLoading: state.isLoading,
                 onPressed: () => context.goNamed(AppRoute.checkout.name),
               ),
             ),
