068e6e1f2baaff6ceddd3902e611e5faec5b7b1e
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-29 10:41:35 +0100

    Implement _moveItemsToRemoteCart

--- ecommerce_app/lib/src/features/cart/application/cart_sync_service.dart
+++ ecommerce_app/lib/src/features/cart/application/cart_sync_service.dart
@@ -1,6 +1,9 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
-import 'package:flutter/foundation.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:ecommerce_app/src/features/cart/domain/mutable_cart.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartSyncService {
@@ -15,9 +18,29 @@ class CartSyncService {
       final previousUser = previous?.value;
       final user = next.value;
       if (previousUser == null && user != null) {
-        debugPrint('signed in: ${user.uid}');
+        _moveItemsToRemoteCart(user.uid);
       }
     });
+  }
+
+  /// moves all items from the local to the remote cart taking into account the
+  /// available quantities
+  Future<void> _moveItemsToRemoteCart(String uid) async {
+    // Get the local cart data
+    final localCartRepository = ref.read(localCartRepositoryProvider);
+    final localCart = await localCartRepository.fetchCart();
+    if (localCart.items.isNotEmpty) {
+      // Get the remote cart data
+      final remoteCartRepository = ref.read(remoteCartRepositoryProvider);
+      final remoteCart = await remoteCartRepository.fetchCart(uid);
+      final localItemsToAdd = localCart.toItemsList();
+      // Add all the local items to the remote cart
+      final updatedRemoteCart = remoteCart.addItems(localItemsToAdd);
+      // Write the updated remote cart data to the repository
+      await remoteCartRepository.setCart(uid, updatedRemoteCart);
+      // Remove all items from the local cart
+      await localCartRepository.setCart(const Cart());
+    }
   }
 }
 
