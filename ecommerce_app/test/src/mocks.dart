265e30c7e14af4b865c124bef6729912d642690c
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-29 14:08:27 +0100

    Add CartSyncService tests

--- ecommerce_app/test/src/mocks.dart
+++ ecommerce_app/test/src/mocks.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_app/src/features/authentication/data/fake_auth_reposit
 import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockAuthRepository extends Mock implements FakeAuthRepository {}
@@ -11,3 +12,5 @@ class MockRemoteCartRepository extends Mock implements RemoteCartRepository {}
 class MockLocalCartRepository extends Mock implements LocalCartRepository {}
 
 class MockCartService extends Mock implements CartService {}
+
+class MockProductsRepository extends Mock implements FakeProductsRepository {}
