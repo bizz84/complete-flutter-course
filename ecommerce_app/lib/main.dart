aa131d7a544f3f52ac3a2f606e53ee65d6ab7465
Andrea Bizzotto <codewithandrea@gmail.com>
2022-06-29 10:05:21 +0100

    Add ProviderContainer and call to container.read() in main.dart
    # Conflicts:
    #	ecommerce_app/lib/main.dart

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_sync_service.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/sembast_cart_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
@@ -16,11 +17,17 @@ void main() async {
   // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
   final localCartRepository = await SembastCartRepository.makeDefault();
-  // * Entry point of the app
-  runApp(ProviderScope(
+  // * Create ProviderContainer with any required overrides
+  final container = ProviderContainer(
     overrides: [
       localCartRepositoryProvider.overrideWithValue(localCartRepository),
     ],
+  );
+  // * Initialize CartSyncService to start the listener
+  container.read(cartSyncServiceProvider);
+  // * Entry point of the app
+  runApp(UncontrolledProviderScope(
+    container: container,
     child: const MyApp(),
   ));
 }
