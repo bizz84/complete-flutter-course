22e64ad0c8e4675e385db671099b7c9cfac582ea
Andrea Bizzotto <codewithandrea@gmail.com>
2022-07-06 11:16:41 +0100

    Starter project section 9
    # Conflicts:
    #	ecommerce_app/lib/src/features/orders/presentation/orders_list/orders_list_screen.dart

---
+++ ecommerce_app/lib/src/features/checkout/application/fake_checkout_service.dart
@@ -0,0 +1,69 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:ecommerce_app/src/features/orders/data/fake_orders_repository.dart';
+import 'package:ecommerce_app/src/features/orders/domain/order.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// A fake checkout service that doesn't process real payments.
+class FakeCheckoutService {
+  FakeCheckoutService(this.ref);
+  final Ref ref;
+
+  /// Temporary client-side logic for placing an order.
+  /// Part of this logic should run on the server, so that we can:
+  /// - setup a payment intent
+  /// - show the payment UI
+  /// - process the payment and fullfill the order
+  /// The server-side logic will be covered in course #2
+  Future<void> placeOrder() async {
+    final authRepository = ref.read(authRepositoryProvider);
+    final remoteCartRepository = ref.read(remoteCartRepositoryProvider);
+    final ordersRepository = ref.read(ordersRepositoryProvider);
+    // * Assertion operator is ok here since this method is only called from
+    // * a place where the user is signed in
+    final uid = authRepository.currentUser!.uid;
+    // 1. Get the cart object
+    final cart = await remoteCartRepository.fetchCart(uid);
+    final total = _totalPrice(cart);
+    // * If we want to make this code more testable, a DateTime builder
+    // * should be injected as a dependency
+    final orderDate = DateTime.now();
+    // * The orderId is a unique string that could be generated with the UUID
+    // * package. Since this is a fake service, we just derive it from the date.
+    final orderId = orderDate.toIso8601String();
+    // 2. Create an order
+    final order = Order(
+      id: orderId,
+      userId: uid,
+      items: cart.items,
+      orderStatus: OrderStatus.confirmed,
+      orderDate: orderDate,
+      total: total,
+    );
+    // 3. Save it using the repository
+    await ordersRepository.addOrder(uid, order);
+    // 4. Empty the cart
+    await remoteCartRepository.setCart(uid, const Cart());
+  }
+
+  // Helper method to calculate the total price
+  double _totalPrice(Cart cart) {
+    if (cart.items.isEmpty) {
+      return 0.0;
+    }
+    final producsRepository = ref.read(productsRepositoryProvider);
+    return cart.items.entries
+        // first extract quantity * price for each item
+        .map((entry) =>
+            entry.value * // quantity
+            producsRepository.getProduct(entry.key)!.price) // price
+        // then add them up
+        .reduce((value, element) => value + element);
+  }
+}
+
+final checkoutServiceProvider = Provider<FakeCheckoutService>((ref) {
+  return FakeCheckoutService(ref);
+});
---
+++ ecommerce_app/lib/src/features/orders/application/user_orders_provider.dart
@@ -0,0 +1,17 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/orders/data/fake_orders_repository.dart';
+import 'package:ecommerce_app/src/features/orders/domain/order.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Watch the list of user orders
+/// NOTE: Only watch this provider if the user is signed in.
+final userOrdersProvider = StreamProvider.autoDispose<List<Order>>((ref) {
+  final user = ref.watch(authStateChangesProvider).value;
+  if (user != null) {
+    final ordersRepository = ref.watch(ordersRepositoryProvider);
+    return ordersRepository.watchUserOrders(user.uid);
+  } else {
+    // If the user is null, just return an empty screen.
+    return const Stream.empty();
+  }
+});
