35d0958684f2a82a588bf1d876ccb71f57ccc8b7
Andrea Bizzotto <codewithandrea@gmail.com>
2022-07-11 11:38:31 +0100

    Update CheckoutScreen to initialize the PageController in initState

--- ecommerce_app/lib/src/features/checkout/presentation/checkout_screen/checkout_screen.dart
+++ ecommerce_app/lib/src/features/checkout/presentation/checkout_screen/checkout_screen.dart
@@ -1,8 +1,10 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
 import 'package:ecommerce_app/src/features/checkout/presentation/payment/payment_page.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// The two sub-routes that are presented as part of the checkout flow.
 enum CheckoutSubRoute { register, payment }
@@ -10,19 +12,27 @@ enum CheckoutSubRoute { register, payment }
 /// This is the root widget of the checkout flow, which is composed of 2 pages:
 /// 1. Register page
 /// 2. Payment page
-/// TODO: Show the correct page based on whether the user is signed in.
-class CheckoutScreen extends StatefulWidget {
+class CheckoutScreen extends ConsumerStatefulWidget {
   const CheckoutScreen({super.key});
 
   @override
-  State<CheckoutScreen> createState() => _CheckoutScreenState();
+  ConsumerState<CheckoutScreen> createState() => _CheckoutScreenState();
 }
 
-class _CheckoutScreenState extends State<CheckoutScreen> {
-  final _controller = PageController();
+class _CheckoutScreenState extends ConsumerState<CheckoutScreen> {
+  late final PageController _controller;
 
   var _subRoute = CheckoutSubRoute.register;
-  // TODO: Load the correct initial page when this screen is presented
+
+  @override
+  void initState() {
+    super.initState();
+    final user = ref.read(authRepositoryProvider).currentUser;
+    if (user != null) {
+      setState(() => _subRoute = CheckoutSubRoute.payment);
+    }
+    _controller = PageController(initialPage: _subRoute.index);
+  }
 
   @override
   void dispose() {
