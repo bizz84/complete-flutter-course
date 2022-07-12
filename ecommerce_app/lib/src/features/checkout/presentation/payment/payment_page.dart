ccbbaad5df86120254fbf4c85e7031353e664187
Andrea Bizzotto <codewithandrea@gmail.com>
2022-07-12 10:23:10 +0100

    Updated the PaymentPage

--- ecommerce_app/lib/src/features/checkout/presentation/payment/payment_page.dart
+++ ecommerce_app/lib/src/features/checkout/presentation/payment/payment_page.dart
@@ -1,41 +1,34 @@
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_items_builder.dart';
 import 'package:ecommerce_app/src/features/checkout/presentation/payment/payment_button.dart';
-import 'package:ecommerce_app/src/features/cart/domain/item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Payment screen showing the items in the cart (with read-only quantities) and
 /// a button to checkout.
-class PaymentPage extends StatelessWidget {
+class PaymentPage extends ConsumerWidget {
   const PaymentPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     // TODO: Listen to cart changes on checkout and update the UI.
-    // TODO: Read from data source
-    const cartItemsList = [
-      Item(
-        productId: '1',
-        quantity: 1,
-      ),
-      Item(
-        productId: '2',
-        quantity: 2,
-      ),
-      Item(
-        productId: '3',
-        quantity: 3,
-      ),
-    ];
-
-    return ShoppingCartItemsBuilder(
-      items: cartItemsList,
-      itemBuilder: (_, item, index) => ShoppingCartItem(
-        item: item,
-        itemIndex: index,
-        isEditable: false,
-      ),
-      ctaBuilder: (_) => const PaymentButton(),
+    final cartValue = ref.watch(cartProvider);
+    return AsyncValueWidget<Cart>(
+      value: cartValue,
+      data: (cart) {
+        return ShoppingCartItemsBuilder(
+          items: cart.toItemsList(),
+          itemBuilder: (_, item, index) => ShoppingCartItem(
+            item: item,
+            itemIndex: index,
+            isEditable: false,
+          ),
+          ctaBuilder: (_) => const PaymentButton(),
+        );
+      },
     );
   }
 }
