6b30768f36f65e497de7ae4359770444c05509bd
Andrea Bizzotto <codewithandrea@gmail.com>
2022-07-13 08:46:26 +0100

    Update unit, widget, integration tests

--- ecommerce_app/test/src/robot.dart
+++ ecommerce_app/test/src/robot.dart
@@ -12,6 +12,8 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'features/authentication/auth_robot.dart';
 import 'features/cart/cart_robot.dart';
+import 'features/checkout/checkout_robot.dart';
+import 'features/orders/orders_robot.dart';
 import 'features/products/products_robot.dart';
 import 'goldens/golden_robot.dart';
 
@@ -20,11 +22,15 @@ class Robot {
       : auth = AuthRobot(tester),
         products = ProductsRobot(tester),
         cart = CartRobot(tester),
+        checkout = CheckoutRobot(tester),
+        orders = OrdersRobot(tester),
         golden = GoldenRobot(tester);
   final WidgetTester tester;
   final AuthRobot auth;
   final ProductsRobot products;
   final CartRobot cart;
+  final CheckoutRobot checkout;
+  final OrdersRobot orders;
   final GoldenRobot golden;
 
   Future<void> pumpMyApp() async {
