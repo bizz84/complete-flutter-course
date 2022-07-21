498a88d4c79e27064483a45be5511e3a7ac5bb3a
Andrea Bizzotto <codewithandrea@gmail.com>
2022-07-21 09:41:25 +0100

    Show user-friendly error message in AsyncValueUI
    # Conflicts:
    #	ecommerce_app/lib/src/utils/async_value_ui.dart

--- ecommerce_app/lib/src/utils/async_value_ui.dart
+++ ecommerce_app/lib/src/utils/async_value_ui.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/exceptions/app_exception.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,11 +7,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 extension AsyncValueUI on AsyncValue {
   void showAlertDialogOnError(BuildContext context) {
     if (!isLoading && hasError) {
+      final message = _errorMessage(error);
       showExceptionAlertDialog(
         context: context,
         title: 'Error'.hardcoded,
-        exception: error,
+        exception: message,
       );
+    }
+  }
+
+  String _errorMessage(Object? error) {
+    if (error is AppException) {
+      return error.details.message;
+    } else {
+      return error.toString();
     }
   }
 }
