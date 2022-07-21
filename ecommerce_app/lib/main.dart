e6961ff4f301cacdeacb642473ddbdca12602929
Andrea Bizzotto <codewithandrea@gmail.com>
2022-07-21 12:01:04 +0100

    Add ErrorLogger class and use it inside main, CartSyncService
    # Conflicts:
    #	ecommerce_app/lib/main.dart
    
    # Conflicts:
    #	ecommerce_app/lib/main.dart

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/exceptions/async_error_logger.dart';
+import 'package:ecommerce_app/src/exceptions/error_logger.dart';
 import 'package:ecommerce_app/src/features/cart/application/cart_sync_service.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/sembast_cart_repository.dart';
@@ -16,7 +17,6 @@ void main() async {
   usePathUrlStrategy();
   // * Register error handlers. For more info, see:
   // * https://docs.flutter.dev/testing/errors
-  registerErrorHandlers();
   final localCartRepository = await SembastCartRepository.makeDefault();
   // * Create ProviderContainer with any required overrides
   final container = ProviderContainer(
@@ -27,6 +27,10 @@ void main() async {
   );
   // * Initialize CartSyncService to start the listener
   container.read(cartSyncServiceProvider);
+  final errorLogger = container.read(errorLoggerProvider);
+  // * Register error handlers. For more info, see:
+  // * https://docs.flutter.dev/testing/errors
+  registerErrorHandlers(errorLogger);
   // * Entry point of the app
   runApp(UncontrolledProviderScope(
     container: container,
@@ -34,15 +38,15 @@ void main() async {
   ));
 }
 
-void registerErrorHandlers() {
+void registerErrorHandlers(ErrorLogger errorLogger) {
   // * Show some error UI if any uncaught exception happens
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.presentError(details);
-    debugPrint(details.toString());
+    errorLogger.logError(details.exception, details.stack);
   };
   // * Handle errors from the underlying platform/OS
   PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
-    debugPrint(error.toString());
+    errorLogger.logError(error, stack);
     return true;
   };
   // * Show some error UI when any widget in the app fails to build
