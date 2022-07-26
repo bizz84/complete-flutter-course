b1801f5166ead44901599a1c50fc3b31dc25c952
Andrea Bizzotto <codewithandrea@gmail.com>
2022-07-26 06:50:54 +0100

    Revert "Install multiple_result and use Result in the FakeAuthRepository"
    This reverts commit 2686167c9d1a40ed2de9abbde13f6ec7a9a9d397.

--- ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -4,7 +4,6 @@ import 'package:ecommerce_app/src/features/authentication/domain/fake_app_user.d
 import 'package:ecommerce_app/src/utils/delay.dart';
 import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:multiple_result/multiple_result.dart';
 
 class FakeAuthRepository {
   FakeAuthRepository({this.addDelay = true});
@@ -17,40 +16,38 @@ class FakeAuthRepository {
   // List to keep track of all user accounts
   final List<FakeAppUser> _users = [];
 
-  Future<Result<AppException, void>> signInWithEmailAndPassword(
-      String email, String password) async {
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
     await delay(addDelay);
     // check the given credentials agains each registered user
     for (final u in _users) {
       // matching email and password
       if (u.email == email && u.password == password) {
         _authState.value = u;
-        return const Success(null);
+        return;
       }
       // same email, wrong password
       if (u.email == email && u.password != password) {
-        return const Error(AppException.wrongPassword());
+        throw const AppException.wrongPassword();
       }
     }
-    return const Error(AppException.userNotFound());
+    throw const AppException.userNotFound();
   }
 
-  Future<Result<AppException, void>> createUserWithEmailAndPassword(
+  Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
     await delay(addDelay);
     // check if the email is already in use
     for (final u in _users) {
       if (u.email == email) {
-        return const Error(AppException.emailAlreadyInUse());
+        throw const AppException.emailAlreadyInUse();
       }
     }
     // minimum password length requirement
     if (password.length < 8) {
-      return const Error(AppException.userNotFound());
+      throw const AppException.userNotFound();
     }
     // create new user
     _createNewUser(email, password);
-    return const Success(null);
   }
 
   Future<void> signOut() async {
