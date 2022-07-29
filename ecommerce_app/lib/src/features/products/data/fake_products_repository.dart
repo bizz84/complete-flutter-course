f514627a077b82188d73223e00007c8976566922
Andrea Bizzotto <codewithandrea@gmail.com>
2022-07-29 16:39:59 +0100

    Starter project for section 11
    # Conflicts:
    #	ecommerce_app/lib/src/app.dart
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/lib/src/features/products/presentation/product_screen/leave_review_action.dart

--- ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
+++ ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
@@ -1,33 +1,52 @@
+import 'dart:async';
+
 import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/utils/delay.dart';
+import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeProductsRepository {
   FakeProductsRepository({this.addDelay = true});
   final bool addDelay;
-  final List<Product> _products = kTestProducts;
+
+  /// Preload with the default list of products when the app starts
+  final _products = InMemoryStore<List<Product>>(List.from(kTestProducts));
 
   List<Product> getProductsList() {
-    return _products;
+    return _products.value;
   }
 
   Product? getProduct(String id) {
-    return _getProduct(_products, id);
+    return _getProduct(_products.value, id);
   }
 
   Future<List<Product>> fetchProductsList() async {
     await delay(addDelay);
-    return Future.value(_products);
+    return Future.value(_products.value);
   }
 
-  Stream<List<Product>> watchProductsList() async* {
-    await delay(addDelay);
-    yield _products;
+  Stream<List<Product>> watchProductsList() {
+    return _products.stream;
   }
 
   Stream<Product?> watchProduct(String id) {
     return watchProductsList().map((products) => _getProduct(products, id));
+  }
+
+  /// Update product or add a new one
+  Future<void> setProduct(Product product) async {
+    await delay(addDelay);
+    final products = _products.value;
+    final index = products.indexWhere((p) => p.id == product.id);
+    if (index == -1) {
+      // if not found, add as a new product
+      products.add(product);
+    } else {
+      // else, overwrite previous product
+      products[index] = product;
+    }
+    _products.value = products;
   }
 
   static Product? _getProduct(List<Product> products, String id) {
