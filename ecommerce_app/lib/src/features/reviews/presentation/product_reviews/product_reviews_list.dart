147935796305c1a0ccd0533948396cdf5390e5cf
Andrea Bizzotto <codewithandrea@gmail.com>
2022-08-05 10:21:15 +0100

    Update ProductReviewsList to show reviews from the repository
    # Conflicts:
    #	ecommerce_app/lib/src/features/reviews/presentation/product_reviews/product_reviews_list.dart

--- ecommerce_app/lib/src/features/reviews/presentation/product_reviews/product_reviews_list.dart
+++ ecommerce_app/lib/src/features/reviews/presentation/product_reviews/product_reviews_list.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
 import 'package:ecommerce_app/src/constants/breakpoints.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/reviews/data/fake_reviews_repository.dart';
 import 'package:ecommerce_app/src/features/reviews/presentation/product_reviews/product_review_card.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
@@ -13,28 +15,19 @@ class ProductReviewsList extends ConsumerWidget {
   final ProductID productId;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: Read from data source
-    final reviews = <Review>[
-      // Review(
-      //   date: DateTime(2022, 2, 12),
-      //   rating: 4.5,
-      //   comment: 'Great product, would buy again!',
-      // ),
-      // Review(
-      //   date: DateTime(2022, 2, 10),
-      //   rating: 4.0,
-      //   comment: 'Looks great but the packaging was damaged.',
-      // ),
-    ];
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (BuildContext context, int index) => ResponsiveCenter(
-          maxContentWidth: Breakpoint.tablet,
-          padding: const EdgeInsets.symmetric(
-              horizontal: Sizes.p16, vertical: Sizes.p8),
-          child: ProductReviewCard(reviews[index]),
+    final reviewsValue = ref.watch(productReviewsProvider(productId));
+    return AsyncValueSliverWidget<List<Review>>(
+      value: reviewsValue,
+      data: (reviews) => SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (BuildContext context, int index) => ResponsiveCenter(
+            maxContentWidth: Breakpoint.tablet,
+            padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.p16, vertical: Sizes.p8),
+            child: ProductReviewCard(reviews[index]),
+          ),
+          childCount: reviews.length,
         ),
-        childCount: reviews.length,
       ),
     );
   }
