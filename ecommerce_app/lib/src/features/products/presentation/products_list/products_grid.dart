3e0f255809f317b00e0fb5076c53996c0faf4405
Andrea Bizzotto <codewithandrea@gmail.com>
2022-08-11 12:24:25 +0100

    Working search implementation

--- ecommerce_app/lib/src/features/products/presentation/products_list/products_grid.dart
+++ ecommerce_app/lib/src/features/products/presentation/products_list/products_grid.dart
@@ -1,9 +1,9 @@
 import 'dart:math';
 
 import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
-import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/features/products/presentation/products_list/product_card.dart';
+import 'package:ecommerce_app/src/features/products/presentation/products_list/products_search_state_provider.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
@@ -18,7 +18,7 @@ class ProductsGrid extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final productsListValue = ref.watch(productsListStreamProvider);
+    final productsListValue = ref.watch(productsSearchResultsProvider);
     return AsyncValueWidget<List<Product>>(
       value: productsListValue,
       data: (products) => products.isEmpty
