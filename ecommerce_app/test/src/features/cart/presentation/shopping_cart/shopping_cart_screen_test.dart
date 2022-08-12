348398c11f3686927a807d22f781973c0e719a54
Andrea Bizzotto <codewithandrea@gmail.com>
2022-08-12 11:29:36 +0100

    Cleanup productsListSearchProvider, tweak productsSearchResultsProvider, fix failing tests
    # Conflicts:
    #	ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
    
    # Conflicts:
    #	ecommerce_app/lib/src/features/products/data/fake_products_repository.dart

--- ecommerce_app/test/src/features/cart/presentation/shopping_cart/shopping_cart_screen_test.dart
+++ ecommerce_app/test/src/features/cart/presentation/shopping_cart/shopping_cart_screen_test.dart
@@ -3,118 +3,140 @@ import '../../../../robot.dart';
 
 void main() {
   group('shopping cart', () {
+    // * Note: All tests are wrapped with `runAsync` to prevent this error:
+    // * A Timer is still pending even after the widget tree was disposed.
     testWidgets('Empty shopping cart', (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      r.products.expectFindNProductCards(14); // check all products are found
-      await r.cart.openCart();
-      r.cart.expectShoppingCartIsEmpty();
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        r.products.expectFindNProductCards(14); // check all products are found
+        await r.cart.openCart();
+        r.cart.expectShoppingCartIsEmpty();
+      });
     });
 
     testWidgets('Add product with quantity = 1', (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      await r.products.selectProduct();
-      await r.cart.addToCart();
-      await r.cart.openCart();
-      r.cart.expectItemQuantity(quantity: 1, atIndex: 0);
-      r.cart.expectShoppingCartTotalIs('Total: \$15.00');
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        await r.products.selectProduct();
+        await r.cart.addToCart();
+        await r.cart.openCart();
+        r.cart.expectItemQuantity(quantity: 1, atIndex: 0);
+        r.cart.expectShoppingCartTotalIs('Total: \$15.00');
+      });
     });
 
     testWidgets('Add product with quantity = 5', (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      await r.products.selectProduct();
-      await r.products.setProductQuantity(5);
-      await r.cart.addToCart();
-      await r.cart.openCart();
-      r.cart.expectItemQuantity(quantity: 5, atIndex: 0);
-      r.cart.expectShoppingCartTotalIs('Total: \$75.00');
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        await r.products.selectProduct();
+        await r.products.setProductQuantity(5);
+        await r.cart.addToCart();
+        await r.cart.openCart();
+        r.cart.expectItemQuantity(quantity: 5, atIndex: 0);
+        r.cart.expectShoppingCartTotalIs('Total: \$75.00');
+      });
     });
 
     testWidgets('Add product with quantity = 6', (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      await r.products.selectProduct();
-      await r.products.setProductQuantity(6);
-      await r.cart.addToCart();
-      await r.cart.openCart();
-      r.cart.expectItemQuantity(quantity: 5, atIndex: 0);
-      r.cart.expectShoppingCartTotalIs('Total: \$75.00');
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        await r.products.selectProduct();
+        await r.products.setProductQuantity(6);
+        await r.cart.addToCart();
+        await r.cart.openCart();
+        r.cart.expectItemQuantity(quantity: 5, atIndex: 0);
+        r.cart.expectShoppingCartTotalIs('Total: \$75.00');
+      });
     });
 
     testWidgets('Add product with quantity = 2, then increment by 2',
         (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      await r.products.selectProduct();
-      await r.products.setProductQuantity(2);
-      await r.cart.addToCart();
-      await r.cart.openCart();
-      await r.cart.incrementCartItemQuantity(quantity: 2, atIndex: 0);
-      r.cart.expectItemQuantity(quantity: 4, atIndex: 0);
-      r.cart.expectShoppingCartTotalIs('Total: \$60.00');
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        await r.products.selectProduct();
+        await r.products.setProductQuantity(2);
+        await r.cart.addToCart();
+        await r.cart.openCart();
+        await r.cart.incrementCartItemQuantity(quantity: 2, atIndex: 0);
+        r.cart.expectItemQuantity(quantity: 4, atIndex: 0);
+        r.cart.expectShoppingCartTotalIs('Total: \$60.00');
+      });
     });
 
     testWidgets('Add product with quantity = 5, then decrement by 2',
         (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      await r.products.selectProduct();
-      await r.products.setProductQuantity(5);
-      await r.cart.addToCart();
-      await r.cart.openCart();
-      await r.cart.decrementCartItemQuantity(quantity: 2, atIndex: 0);
-      r.cart.expectItemQuantity(quantity: 3, atIndex: 0);
-      r.cart.expectShoppingCartTotalIs('Total: \$45.00');
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        await r.products.selectProduct();
+        await r.products.setProductQuantity(5);
+        await r.cart.addToCart();
+        await r.cart.openCart();
+        await r.cart.decrementCartItemQuantity(quantity: 2, atIndex: 0);
+        r.cart.expectItemQuantity(quantity: 3, atIndex: 0);
+        r.cart.expectShoppingCartTotalIs('Total: \$45.00');
+      });
     });
 
     testWidgets('Add two products', (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      // add first product
-      await r.products.selectProduct(atIndex: 0);
-      await r.cart.addToCart();
-      await r.goBack();
-      // add second product
-      await r.products.selectProduct(atIndex: 1);
-      await r.cart.addToCart();
-      await r.cart.openCart();
-      r.cart.expectFindNCartItems(2);
-      r.cart.expectShoppingCartTotalIs('Total: \$28.00');
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        // add first product
+        await r.products.selectProduct(atIndex: 0);
+        await r.cart.addToCart();
+        await r.goBack();
+        // add second product
+        await r.products.selectProduct(atIndex: 1);
+        await r.cart.addToCart();
+        await r.cart.openCart();
+        r.cart.expectFindNCartItems(2);
+        r.cart.expectShoppingCartTotalIs('Total: \$28.00');
+      });
     });
 
     testWidgets('Add product, then delete it', (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      await r.products.selectProduct();
-      await r.cart.addToCart();
-      await r.cart.openCart();
-      await r.cart.deleteCartItem(atIndex: 0);
-      r.cart.expectShoppingCartIsEmpty();
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        await r.products.selectProduct();
+        await r.cart.addToCart();
+        await r.cart.openCart();
+        await r.cart.deleteCartItem(atIndex: 0);
+        r.cart.expectShoppingCartIsEmpty();
+      });
     });
 
     testWidgets('Add product with quantity = 5, goes out of stock',
         (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      await r.products.selectProduct();
-      await r.products.setProductQuantity(5);
-      await r.cart.addToCart();
-      r.cart.expectProductIsOutOfStock();
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        await r.products.selectProduct();
+        await r.products.setProductQuantity(5);
+        await r.cart.addToCart();
+        r.cart.expectProductIsOutOfStock();
+      });
     });
 
     testWidgets(
         'Add product with quantity = 5, remains out of stock when opened again',
         (tester) async {
-      final r = Robot(tester);
-      await r.pumpMyApp();
-      await r.products.selectProduct();
-      await r.products.setProductQuantity(5);
-      await r.cart.addToCart();
-      await r.goBack();
-      await r.products.selectProduct();
-      r.cart.expectProductIsOutOfStock();
+      await tester.runAsync(() async {
+        final r = Robot(tester);
+        await r.pumpMyApp();
+        await r.products.selectProduct();
+        await r.products.setProductQuantity(5);
+        await r.cart.addToCart();
+        await r.goBack();
+        await r.products.selectProduct();
+        r.cart.expectProductIsOutOfStock();
+      });
     });
   });
 }
