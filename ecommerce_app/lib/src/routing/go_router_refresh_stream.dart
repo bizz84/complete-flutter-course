29a91a6b93055229c4f68a1b24ad81da62da9e50
Andrea Bizzotto <codewithandrea@gmail.com>
2022-09-22 10:11:53 +0100

    Update project to GoRouter 5.0 (#22)
    * Add GoRouterRefreshStream as a separate class, use it in redirect code
    
    * Pass goRouter to routerConfig argument when creating MaterialApp.router
    
    * Update to GoRouter 5.0
    
    * Add guide on how to migrate course project from GoRouter 4.x to 5.0
    
    * More info about GoRouter 5.0 migration

---
+++ ecommerce_app/lib/src/routing/go_router_refresh_stream.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+
+/// This class was included
+class GoRouterRefreshStream extends ChangeNotifier {
+  GoRouterRefreshStream(Stream<dynamic> stream) {
+    notifyListeners();
+    _subscription = stream.asBroadcastStream().listen(
+          (dynamic _) => notifyListeners(),
+        );
+  }
+
+  late final StreamSubscription<dynamic> _subscription;
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+}
