413932dd7a578137f0070ad91d38e4b3c1aae701
Andrea Bizzotto <codewithandrea@gmail.com>
2022-09-26 11:40:33 +0100

    Install and setup GoRouter 14.4.1
    # Conflicts:
    #	ecommerce_app/lib/main.dart
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+// ignore:depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // turn off the # in the URLs on the web
+  usePathUrlStrategy();
   // * Register error handlers. For more info, see:
   // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
