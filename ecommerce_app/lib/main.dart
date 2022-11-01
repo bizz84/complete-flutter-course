c6c610701c531554d02c68613c2429d4ab6fd8d1
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-01 09:10:40 +0000

    Updated error handling code to use PlatformDispatcher.instance.onError, removed runZonedGuarded
    # Conflicts:
    #	ecommerce_app/lib/main.dart

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,32 +1,36 @@
-import 'dart:async';
-
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  // * For more info on error handling, see:
+  WidgetsFlutterBinding.ensureInitialized();
+  // * Register error handlers. For more info, see:
   // * https://docs.flutter.dev/testing/errors
-  await runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    // * Entry point of the app
-    runApp(const MyApp());
+  registerErrorHandlers();
+  // * Entry point of the app
+  runApp(const MyApp());
+}
 
-    // * This code will present some error UI if any uncaught exception happens
-    FlutterError.onError = (FlutterErrorDetails details) {
-      FlutterError.presentError(details);
-    };
-    ErrorWidget.builder = (FlutterErrorDetails details) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('An error occurred'.hardcoded),
-        ),
-        body: Center(child: Text(details.toString())),
-      );
-    };
-  }, (Object error, StackTrace stack) {
-    // * Log any errors to console
+void registerErrorHandlers() {
+  // * Show some error UI if any uncaught exception happens
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    debugPrint(details.toString());
+  };
+  // * Handle errors from the underlying platform/OS
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
     debugPrint(error.toString());
-  });
+    return true;
+  };
+  // * Show some error UI when any widget in the app fails to build
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('An error occurred'.hardcoded),
+      ),
+      body: Center(child: Text(details.toString())),
+    );
+  };
 }
