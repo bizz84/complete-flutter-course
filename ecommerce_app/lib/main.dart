97ce7a35212a054e8514c7c348678c4ba890bbb5
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-01 10:27:21 +0000

    Install and setup flutter_riverpod: 2.6.1
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 // ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 
@@ -13,7 +14,7 @@ void main() async {
   // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
   // * Entry point of the app
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 void registerErrorHandlers() {
