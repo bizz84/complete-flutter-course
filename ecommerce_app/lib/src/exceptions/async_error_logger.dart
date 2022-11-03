45201fa584047fa68e5bc086b686105d54ada523
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-03 08:33:42 +0000

    Remove EmailPasswordSignInState, refactor email & password feature to use AsyncValue as state
    # Conflicts:
    #	ecommerce_app/test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart

--- ecommerce_app/lib/src/exceptions/async_error_logger.dart
+++ ecommerce_app/lib/src/exceptions/async_error_logger.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerce_app/src/exceptions/app_exception.dart';
 import 'package:ecommerce_app/src/exceptions/error_logger.dart';
-import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Error logger class to keep track of all AsyncError states that are set
@@ -27,9 +26,7 @@ class AsyncErrorLogger extends ProviderObserver {
   }
 
   AsyncError<dynamic>? _findError(Object? value) {
-    if (value is EmailPasswordSignInState && value.value is AsyncError) {
-      return value.value as AsyncError;
-    } else if (value is AsyncError) {
+    if (value is AsyncError) {
       return value;
     } else {
       return null;
