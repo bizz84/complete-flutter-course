59b4adc85431cda3434c8d97b456192fb7cb112f
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-03 08:33:42 +0000

    Remove EmailPasswordSignInState, refactor email & password feature to use AsyncValue as state

--- ecommerce_app/lib/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_controller.dart';
-import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_form_type.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_validators.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/string_validators.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/utils/async_value_ui.dart';
@@ -52,7 +53,8 @@ class EmailPasswordSignInContents extends ConsumerStatefulWidget {
 }
 
 class _EmailPasswordSignInContentsState
-    extends ConsumerState<EmailPasswordSignInContents> {
+    extends ConsumerState<EmailPasswordSignInContents>
+    with EmailAndPasswordValidators {
   final _formKey = GlobalKey<FormState>();
   final _node = FocusScopeNode();
   final _emailController = TextEditingController();
@@ -66,6 +68,8 @@ class _EmailPasswordSignInContentsState
   // For more details on how this is implemented, see:
   // https://codewithandrea.com/articles/flutter-text-field-form-validation/
   var _submitted = false;
+  // track the formType as a local state variable
+  late var _formType = widget.formType;
 
   @override
   void dispose() {
@@ -76,38 +80,40 @@ class _EmailPasswordSignInContentsState
     super.dispose();
   }
 
-  Future<void> _submit(EmailPasswordSignInState state) async {
+  Future<void> _submit() async {
     setState(() => _submitted = true);
     // only submit the form if validation passes
     if (_formKey.currentState!.validate()) {
-      final controller = ref.read(
-          emailPasswordSignInControllerProvider(widget.formType).notifier);
-      final success = await controller.submit(email, password);
+      final controller =
+          ref.read(emailPasswordSignInControllerProvider.notifier);
+      final success = await controller.submit(
+        email: email,
+        password: password,
+        formType: _formType,
+      );
       if (success) {
         widget.onSignedIn?.call();
       }
     }
   }
 
-  void _emailEditingComplete(EmailPasswordSignInState state) {
-    if (state.canSubmitEmail(email)) {
+  void _emailEditingComplete() {
+    if (canSubmitEmail(email)) {
       _node.nextFocus();
     }
   }
 
-  void _passwordEditingComplete(EmailPasswordSignInState state) {
-    if (!state.canSubmitEmail(email)) {
+  void _passwordEditingComplete() {
+    if (!canSubmitEmail(email)) {
       _node.previousFocus();
       return;
     }
-    _submit(state);
+    _submit();
   }
 
-  void _updateFormType(EmailPasswordSignInFormType formType) {
+  void _updateFormType() {
     // * Toggle between register and sign in form
-    ref
-        .read(emailPasswordSignInControllerProvider(widget.formType).notifier)
-        .updateFormType(formType);
+    setState(() => _formType = _formType.secondaryActionFormType);
     // * Clear the password field when doing so
     _passwordController.clear();
   }
@@ -115,12 +121,10 @@ class _EmailPasswordSignInContentsState
   @override
   Widget build(BuildContext context) {
     ref.listen<AsyncValue>(
-      emailPasswordSignInControllerProvider(widget.formType)
-          .select((state) => state.value),
+      emailPasswordSignInControllerProvider,
       (_, state) => state.showAlertDialogOnError(context),
     );
-    final state =
-        ref.watch(emailPasswordSignInControllerProvider(widget.formType));
+    final state = ref.watch(emailPasswordSignInControllerProvider);
     return ResponsiveScrollableCard(
       child: FocusScope(
         node: _node,
@@ -141,12 +145,12 @@ class _EmailPasswordSignInContentsState
                 ),
                 autovalidateMode: AutovalidateMode.onUserInteraction,
                 validator: (email) =>
-                    !_submitted ? null : state.emailErrorText(email ?? ''),
+                    !_submitted ? null : emailErrorText(email ?? ''),
                 autocorrect: false,
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.emailAddress,
                 keyboardAppearance: Brightness.light,
-                onEditingComplete: () => _emailEditingComplete(state),
+                onEditingComplete: () => _emailEditingComplete(),
                 inputFormatters: <TextInputFormatter>[
                   ValidatorInputFormatter(
                       editingValidator: EmailEditingRegexValidator()),
@@ -158,31 +162,29 @@ class _EmailPasswordSignInContentsState
                 key: EmailPasswordSignInScreen.passwordKey,
                 controller: _passwordController,
                 decoration: InputDecoration(
-                  labelText: state.passwordLabelText,
+                  labelText: _formType.passwordLabelText,
                   enabled: !state.isLoading,
                 ),
                 autovalidateMode: AutovalidateMode.onUserInteraction,
                 validator: (password) => !_submitted
                     ? null
-                    : state.passwordErrorText(password ?? ''),
+                    : passwordErrorText(password ?? '', _formType),
                 obscureText: true,
                 autocorrect: false,
                 textInputAction: TextInputAction.done,
                 keyboardAppearance: Brightness.light,
-                onEditingComplete: () => _passwordEditingComplete(state),
+                onEditingComplete: () => _passwordEditingComplete(),
               ),
               gapH8,
               PrimaryButton(
-                text: state.primaryButtonText,
+                text: _formType.primaryButtonText,
                 isLoading: state.isLoading,
-                onPressed: state.isLoading ? null : () => _submit(state),
+                onPressed: state.isLoading ? null : () => _submit(),
               ),
               gapH8,
               CustomTextButton(
-                text: state.secondaryButtonText,
-                onPressed: state.isLoading
-                    ? null
-                    : () => _updateFormType(state.secondaryActionFormType),
+                text: _formType.secondaryButtonText,
+                onPressed: state.isLoading ? null : _updateFormType,
               ),
             ],
           ),
--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_form_type.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart';
-import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_screen.dart';
 import 'package:ecommerce_app/src/features/checkout/presentation/checkout_screen/checkout_screen.dart';
 import 'package:ecommerce_app/src/features/orders/presentation/orders_list/orders_list_screen.dart';
