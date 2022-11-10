4d5a59075272757ddaf46dcbe7becb851ccaf3b0
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-10 10:31:03 +0000

    Update currencyFormatter, currentDateBuilder, dateFormatter to use Riverpod Generator

---
+++ ecommerce_app/lib/src/utils/currency_formatter.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'currency_formatter.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$currencyFormatterHash() => r'd62e64f3963ad26588fa58656b90a06f54abfd32';
+
+/// See also [currencyFormatter].
+@ProviderFor(currencyFormatter)
+final currencyFormatterProvider = AutoDisposeProvider<NumberFormat>.internal(
+  currencyFormatter,
+  name: r'currencyFormatterProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$currencyFormatterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CurrencyFormatterRef = AutoDisposeProviderRef<NumberFormat>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
