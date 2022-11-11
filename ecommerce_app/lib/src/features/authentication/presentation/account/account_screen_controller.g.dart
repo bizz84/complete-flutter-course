0502e9252ed47c7bb70fb5feeefbef626885a667
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-11 11:39:54 +0000

    Convert AccountScreenController and EmailPasswordSignInController to use Riverpod Generator

---
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'account_screen_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$accountScreenControllerHash() =>
+    r'214c588739a1c695e32a9a386805299d46a9625a';
+
+/// See also [AccountScreenController].
+@ProviderFor(AccountScreenController)
+final accountScreenControllerProvider =
+    AutoDisposeAsyncNotifierProvider<AccountScreenController, void>.internal(
+  AccountScreenController.new,
+  name: r'accountScreenControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$accountScreenControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AccountScreenController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
