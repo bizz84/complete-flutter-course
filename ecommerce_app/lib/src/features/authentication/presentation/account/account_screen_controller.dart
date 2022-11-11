259fda86fde11fb67ac152b2fa76b48c2e4d74f3
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-11 10:58:22 +0000

    Convert AccountScreenController to use AutoDisposeAsyncNotifier

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,21 +1,20 @@
+import 'dart:async';
+
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue<void>> {
-  AccountScreenController({required this.authRepository})
-      : super(const AsyncData(null));
-  final FakeAuthRepository authRepository;
-
+class AccountScreenController extends AutoDisposeAsyncNotifier<void> {
+  @override
+  FutureOr<void> build() {
+    // nothing to do
+  }
   Future<void> signOut() async {
+    final authRepository = ref.read(authRepositoryProvider);
     state = const AsyncLoading();
     state = await AsyncValue.guard(() => authRepository.signOut());
   }
 }
 
-final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
-    AccountScreenController, AsyncValue<void>>((ref) {
-  final authRepository = ref.watch(authRepositoryProvider);
-  return AccountScreenController(
-    authRepository: authRepository,
-  );
-});
+final accountScreenControllerProvider =
+    AutoDisposeAsyncNotifierProvider<AccountScreenController, void>(
+        AccountScreenController.new);
