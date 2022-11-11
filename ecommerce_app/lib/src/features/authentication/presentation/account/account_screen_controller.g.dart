939b271559d322b36c7675f145fa3c81e5bb77f3
Andrea Bizzotto <codewithandrea@gmail.com>
2022-11-11 11:39:54 +0000

    Convert AccountScreenController and EmailPasswordSignInController to use Riverpod Generator

---
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'account_screen_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$accountScreenControllerHash() =>
+    r'214c588739a1c695e32a9a386805299d46a9625a';
+
+/// See also [AccountScreenController].
+@ProviderFor(AccountScreenController)
+final accountScreenControllerProvider =
+    AutoDisposeAsyncNotifierProvider<AccountScreenController, void>.internal(
+  AccountScreenController.new,
+  name: r'accountScreenControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$accountScreenControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AccountScreenController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
---
+++ ecommerce_app/lib/src/features/authentication/presentation/sign_in/email_password_sign_in_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'email_password_sign_in_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$emailPasswordSignInControllerHash() =>
+    r'9297287fca9087a9a6e5d554d6136e07b3f0cea0';
+
+/// See also [EmailPasswordSignInController].
+@ProviderFor(EmailPasswordSignInController)
+final emailPasswordSignInControllerProvider = AutoDisposeAsyncNotifierProvider<
+    EmailPasswordSignInController, void>.internal(
+  EmailPasswordSignInController.new,
+  name: r'emailPasswordSignInControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$emailPasswordSignInControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EmailPasswordSignInController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
