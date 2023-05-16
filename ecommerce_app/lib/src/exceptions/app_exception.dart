34aa8eb87aeb617e2c99b9fc0005a488ea55bbaf
Andrea Bizzotto <codewithandrea@gmail.com>
2023-05-16 12:15:13 +0100

    Use AppException subclasses in FakeAuthRepository, add toString() method

--- ecommerce_app/lib/src/exceptions/app_exception.dart
+++ ecommerce_app/lib/src/exceptions/app_exception.dart
@@ -5,6 +5,9 @@ sealed class AppException implements Exception {
   AppException(this.code, this.message);
   final String code;
   final String message;
+
+  @override
+  String toString() => message;
 }
 
 /// Auth
