5b59e64d091d2ee0bd0751365d7a2a0351f100fc
Andrea Bizzotto <codewithandrea@gmail.com>
2023-05-16 13:52:20 +0100

    Install multiple_result and use Result in the FakeAuthRepository
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    
    # Conflicts:
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml

--- ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -4,6 +4,7 @@ import 'package:ecommerce_app/src/features/authentication/domain/fake_app_user.d
 import 'package:ecommerce_app/src/utils/delay.dart';
 import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:multiple_result/multiple_result.dart';
 
 class FakeAuthRepository {
   FakeAuthRepository({this.addDelay = true});
@@ -16,38 +17,40 @@ class FakeAuthRepository {
   // List to keep track of all user accounts
   final List<FakeAppUser> _users = [];
 
-  Future<void> signInWithEmailAndPassword(String email, String password) async {
+  Future<Result<void, AppException>> signInWithEmailAndPassword(
+      String email, String password) async {
     await delay(addDelay);
     // check the given credentials agains each registered user
     for (final u in _users) {
       // matching email and password
       if (u.email == email && u.password == password) {
         _authState.value = u;
-        return;
+        return const Success(null);
       }
       // same email, wrong password
       if (u.email == email && u.password != password) {
-        throw WrongPasswordException();
+        return Error(WrongPasswordException());
       }
     }
-    throw UserNotFoundException();
+    return Error(UserNotFoundException());
   }
 
-  Future<void> createUserWithEmailAndPassword(
+  Future<Result<void, AppException>> createUserWithEmailAndPassword(
       String email, String password) async {
     await delay(addDelay);
     // check if the email is already in use
     for (final u in _users) {
       if (u.email == email) {
-        throw EmailAlreadyInUseException();
+        return Error(EmailAlreadyInUseException());
       }
     }
     // minimum password length requirement
     if (password.length < 8) {
-      throw WeakPasswordException();
+      return Error(WeakPasswordException());
     }
     // create new user
     _createNewUser(email, password);
+    return const Success(null);
   }
 
   Future<void> signOut() async {
