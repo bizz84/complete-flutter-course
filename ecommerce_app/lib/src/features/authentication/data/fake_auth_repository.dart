e03e2749bc85a7b9518505f5f86a13e66abd5b78
Andrea Bizzotto <codewithandrea@gmail.com>
2023-05-16 12:15:13 +0100

    Use AppException subclasses in FakeAuthRepository, add toString() method

--- ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,6 +1,6 @@
+import 'package:ecommerce_app/src/exceptions/app_exception.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/fake_app_user.dart';
-import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/utils/delay.dart';
 import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -27,10 +27,10 @@ class FakeAuthRepository {
       }
       // same email, wrong password
       if (u.email == email && u.password != password) {
-        throw Exception('Wrong password'.hardcoded);
+        throw WrongPasswordException();
       }
     }
-    throw Exception('User not found'.hardcoded);
+    throw UserNotFoundException();
   }
 
   Future<void> createUserWithEmailAndPassword(
@@ -39,12 +39,12 @@ class FakeAuthRepository {
     // check if the email is already in use
     for (final u in _users) {
       if (u.email == email) {
-        throw Exception('Email already in use'.hardcoded);
+        throw EmailAlreadyInUseException();
       }
     }
     // minimum password length requirement
     if (password.length < 8) {
-      throw Exception('Password is too weak'.hardcoded);
+      throw WeakPasswordException();
     }
     // create new user
     _createNewUser(email, password);
