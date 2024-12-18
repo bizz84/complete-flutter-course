96041bf86af046a3f3a109e524ff5ae19d90440c
akups <appiah-lindberg@outlook.com>
2024-12-18 05:18:23 +0100

    named routing

--- ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
+++ ecommerce_app/lib/src/features/home_app_bar/more_menu_button.dart
@@ -1,10 +1,7 @@
-import 'package:ecommerce_app/src/features/account/account_screen.dart';
-import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
-import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/models/app_user.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 enum PopupMenuOption {
@@ -54,13 +51,13 @@ class MoreMenuButton extends StatelessWidget {
         // push to different routes based on selected option
         switch (option) {
           case PopupMenuOption.signIn:
-            context.go('/signIn');
+            context.goNamed(AppRoute.signIn.name);
             break;
           case PopupMenuOption.orders:
-            context.go('/orders');
+            context.goNamed(AppRoute.orders.name);
             break;
           case PopupMenuOption.account:
-            context.go('/account');
+            context.goNamed(AppRoute.account.name);
             break;
         }
       },
